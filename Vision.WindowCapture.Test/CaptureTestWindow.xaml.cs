9d8c07a2a9898053b4e0ef5044015c194445372b
huiyadanli <huiyadanli@gmail.com>
2023-10-07 11:26:26 +0800

    Add capture test

---
+++ Vision.WindowCapture.Test/CaptureTestWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Windows.Win32.Foundation;
+
+namespace Vision.WindowCapture.Test
+{
+    /// <summary>
+    /// CaptureTestWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class CaptureTestWindow : Window
+    {
+        private IWindowCapture? _capture;
+        public CaptureTestWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void StartCapture(IntPtr hWnd, CaptureModeEnum captureMode)
+        {
+            if (hWnd == IntPtr.Zero)
+            {
+                MessageBox.Show("请选择窗口");
+                return;
+            }
+
+
+            _capture = WindowCaptureFactory.Create(captureMode);
+            _capture.Start((HWND)hWnd);
+
+            CompositionTarget.Rendering += Loop;
+        }
+
+        private void Loop(object? sender, EventArgs e)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var bitmap = _capture?.Capture();
+            sw.Stop();
+            Debug.WriteLine("截图耗时:" + sw.ElapsedMilliseconds);
+
+            if (bitmap != null)
+            {
+                sw.Reset();
+                sw.Start();
+                DisplayCaptureResultImage.Source = ToBitmapImage(bitmap);
+                sw.Stop();
+                Debug.WriteLine("转换耗时:" + sw.ElapsedMilliseconds);
+            }
+        }
+
+        public static BitmapImage ToBitmapImage( Bitmap bitmap)
+        {
+            var ms = new MemoryStream();
+            bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            var image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+    }
+
+}
---
+++ Vision.WindowCapture.Test/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vision.WindowCapture.Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CaptureBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            var picker = new PickerWindow();
+            var hWnd = picker.PickCaptureTarget(new WindowInteropHelper(this).Handle);
+            if (hWnd != IntPtr.Zero)
+            {
+                var captureWindow = new CaptureTestWindow();
+                captureWindow.StartCapture(hWnd, CaptureModeEnum.WindowsGraphicsCapture);
+                captureWindow.Show();
+            }
+        }
+    }
+}
