9b193e56ea8613bc2737df4854652407986fefc1
ema <mccoy39082@163.com>
2023-11-24 04:56:40 +0800

    feat: now you can launch through `bettergi://`

--- BetterGenshinImpact/App.xaml.cs
+++ BetterGenshinImpact/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using BetterGenshinImpact.Core.Config;
 using BetterGenshinImpact.GameTask;
+using BetterGenshinImpact.Helpers;
 using BetterGenshinImpact.Service;
 using BetterGenshinImpact.Service.Interface;
 using BetterGenshinImpact.View;
@@ -19,248 +20,245 @@ using System.Threading.Tasks;
 using System.Windows;
 using Wpf.Ui;
 
-namespace BetterGenshinImpact
+namespace BetterGenshinImpact;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    // The.NET Generic Host provides dependency injection, configuration, logging, and other services.
+    // https://docs.microsoft.com/dotnet/core/extensions/generic-host
+    // https://docs.microsoft.com/dotnet/core/extensions/dependency-injection
+    // https://docs.microsoft.com/dotnet/core/extensions/configuration
+    // https://docs.microsoft.com/dotnet/core/extensions/logging
+    private static readonly IHost _host = Host.CreateDefaultBuilder()
+        .ConfigureServices(
+            (context, services) =>
+            {
+                // 提前初始化配置
+                var configService = new ConfigService();
+                services.AddSingleton<IConfigService>(sp => configService);
+                var all = configService.Get();
+
+                var logFolder = Path.Combine(AppContext.BaseDirectory, @"log");
+                Directory.CreateDirectory(logFolder);
+                var logFile = Path.Combine(logFolder, $"better-genshin-impact.log");
+
+                var maskWindow = new MaskWindow();
+                services.AddSingleton(maskWindow);
+
+                var loggerConfiguration = new LoggerConfiguration()
+                    .WriteTo.File(path: logFile, outputTemplate: "[{Timestamp:HH:mm:ss.fff}] [{Level:u3}] {SourceContext}{NewLine}{Message}{NewLine}{Exception}{NewLine}", rollingInterval: RollingInterval.Day)
+                    .MinimumLevel.Information()
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                    .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Warning);
+                if (all.MaskWindowConfig.MaskEnabled)
+                {
+                    loggerConfiguration.WriteTo.RichTextBox(maskWindow.LogBox, outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}");
+                }
+                Log.Logger = loggerConfiguration.CreateLogger();
+                services.AddLogging(c => c.AddSerilog());
+
+                // App Host
+                services.AddHostedService<ApplicationHostService>();
+
+                // Page resolver service
+                services.AddSingleton<IPageService, PageService>();
+
+                // Service containing navigation, same as INavigationWindow... but without window
+                services.AddSingleton<INavigationService, NavigationService>();
+
+                // Main window with navigation
+                services.AddSingleton<INavigationWindow, MainWindow>();
+                services.AddSingleton<MainWindowViewModel>();
+
+                // Views and ViewModels
+                services.AddSingleton<HomePage>();
+                services.AddSingleton<HomePageViewModel>();
+                services.AddSingleton<TriggerSettingsPage>();
+                services.AddSingleton<TriggerSettingsPageViewModel>();
+                services.AddSingleton<MacroSettingsPage>();
+                services.AddSingleton<MacroSettingsPageViewModel>();
+                services.AddSingleton<CommonSettingsPage>();
+                services.AddSingleton<CommonSettingsPageViewModel>();
+                services.AddSingleton<TaskSettingsPage>();
+                services.AddSingleton<TaskSettingsPageViewModel>();
+
+                services.AddSingleton<HotKeyPage>();
+                services.AddSingleton<HotKeyPageViewModel>();
+
+                // My Services
+                services.AddSingleton<TaskTriggerDispatcher>();
+
+                // Configuration
+                //services.Configure<AppConfig>(context.Configuration.GetSection(nameof(AppConfig)));
+            }
+        )
+        .Build();
+
+    public static ILogger<T> GetLogger<T>()
     {
-        // The.NET Generic Host provides dependency injection, configuration, logging, and other services.
-        // https://docs.microsoft.com/dotnet/core/extensions/generic-host
-        // https://docs.microsoft.com/dotnet/core/extensions/dependency-injection
-        // https://docs.microsoft.com/dotnet/core/extensions/configuration
-        // https://docs.microsoft.com/dotnet/core/extensions/logging
-        private static readonly IHost _host = Host.CreateDefaultBuilder()
-            .ConfigureServices(
-                (context, services) =>
-                {
-                    // 提前初始化配置
-                    var configService = new ConfigService();
-                    services.AddSingleton<IConfigService>(sp => configService);
-                    var all = configService.Get();
+        return _host.Services.GetService<ILogger<T>>()!;
+    }
 
-                    var logFolder = Path.Combine(AppContext.BaseDirectory, @"log");
-                    Directory.CreateDirectory(logFolder);
-                    var logFile = Path.Combine(logFolder, $"better-genshin-impact.log");
+    /// <summary>
+    /// Gets registered service.
+    /// </summary>
+    /// <typeparam name="T">Type of the service to get.</typeparam>
+    /// <returns>Instance of the service or <see langword="null"/>.</returns>
+    public static T? GetService<T>() where T : class
+    {
+        return _host.Services.GetService(typeof(T)) as T;
+    }
 
-                    var maskWindow = new MaskWindow();
-                    services.AddSingleton(maskWindow);
+    /// <summary>
+    /// Occurs when the application is loading.
+    /// </summary>
+    private void OnStartup(object sender, StartupEventArgs e)
+    {
+        StartUp();
+    }
 
-                    var loggerConfiguration = new LoggerConfiguration()
-                        .WriteTo.File(path: logFile, outputTemplate: "[{Timestamp:HH:mm:ss.fff}] [{Level:u3}] {SourceContext}{NewLine}{Message}{NewLine}{Exception}{NewLine}", rollingInterval: RollingInterval.Day)
-                        .MinimumLevel.Information()
-                        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                        .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Warning);
-                    if (all.MaskWindowConfig.MaskEnabled)
-                    {
-                        loggerConfiguration.WriteTo.RichTextBox(maskWindow.LogBox, outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}");
-                    }
-                    Log.Logger = loggerConfiguration.CreateLogger();
-                    services.AddLogging(c => c.AddSerilog());
-
-                    // App Host
-                    services.AddHostedService<ApplicationHostService>();
-
-                    // Page resolver service
-                    services.AddSingleton<IPageService, PageService>();
-
-                    // Service containing navigation, same as INavigationWindow... but without window
-                    services.AddSingleton<INavigationService, NavigationService>();
-
-                    // Main window with navigation
-                    services.AddSingleton<INavigationWindow, MainWindow>();
-                    services.AddSingleton<MainWindowViewModel>();
-
-                    // Views and ViewModels
-                    services.AddSingleton<HomePage>();
-                    services.AddSingleton<HomePageViewModel>();
-                    services.AddSingleton<TriggerSettingsPage>();
-                    services.AddSingleton<TriggerSettingsPageViewModel>();
-                    services.AddSingleton<MacroSettingsPage>();
-                    services.AddSingleton<MacroSettingsPageViewModel>();
-                    services.AddSingleton<CommonSettingsPage>();
-                    services.AddSingleton<CommonSettingsPageViewModel>();
-                    services.AddSingleton<TaskSettingsPage>();
-                    services.AddSingleton<TaskSettingsPageViewModel>();
-
-                    services.AddSingleton<HotKeyPage>();
-                    services.AddSingleton<HotKeyPageViewModel>();
-
-                    // My Services
-                    services.AddSingleton<TaskTriggerDispatcher>();
-
-                    // Configuration
-                    //services.Configure<AppConfig>(context.Configuration.GetSection(nameof(AppConfig)));
-                }
-            )
-            .Build();
-
-        public static ILogger<T> GetLogger<T>()
+    private async void StartUp()
+    {
+        try
         {
-            return _host.Services.GetService<ILogger<T>>()!;
+            await StartUpCoreAsync();
+            await UrlProtocolHelper.RegisterAsync();
         }
-
-        /// <summary>
-        /// Gets registered service.
-        /// </summary>
-        /// <typeparam name="T">Type of the service to get.</typeparam>
-        /// <returns>Instance of the service or <see langword="null"/>.</returns>
-        public static T? GetService<T>() where T : class
+        catch (Exception ex)
         {
-            return _host.Services.GetService(typeof(T)) as T;
+            // DEBUG only, no overhead
+            Debug.WriteLine(ex);
+
+            if (Debugger.IsAttached)
+            {
+                Debugger.Break();
+            }
         }
+    }
 
-        /// <summary>
-        /// Occurs when the application is loading.
-        /// </summary>
-        private void OnStartup(object sender, StartupEventArgs e)
+    private async Task StartUpCoreAsync()
+    {
+        // 获得当前登录的Windows用户标示
+        var identity = WindowsIdentity.GetCurrent();
+        var principal = new WindowsPrincipal(identity);
+        // 判断当前登录用户是否为管理员
+        if (principal.IsInRole(WindowsBuiltInRole.Administrator))
         {
-            StartUp();
+            //如果是管理员，则直接运行
+            RegisterEvents();
+            await _host.StartAsync();
         }
-
-        private async void StartUp()
+        else
         {
+            //创建启动对象
+            var startInfo = new ProcessStartInfo
+            {
+                UseShellExecute = true,
+                WorkingDirectory = Global.StartUpPath,
+                FileName = "BetterGI.exe",
+                //设置启动动作,确保以管理员身份运行
+                Verb = "runas"
+            };
             try
             {
-                await StartUpCoreAsync();
+                Process.Start(startInfo);
             }
             catch (Exception ex)
             {
-                // DEBUG only, no overhead
                 Debug.WriteLine(ex);
-
-                if (Debugger.IsAttached)
-                {
-                    Debugger.Break();
-                }
+                return;
             }
+
+            //退出
+            Current.Shutdown();
         }
+    }
 
-        private async Task StartUpCoreAsync()
-        {
-            // 获得当前登录的Windows用户标示
-            var identity = WindowsIdentity.GetCurrent();
-            var principal = new WindowsPrincipal(identity);
-            // 判断当前登录用户是否为管理员
-            if (principal.IsInRole(WindowsBuiltInRole.Administrator))
-            {
-                //如果是管理员，则直接运行
-                RegisterEvents();
-                await _host.StartAsync();
-            }
-            else
-            {
-                //创建启动对象
-                var startInfo = new ProcessStartInfo
-                {
-                    UseShellExecute = true,
-                    WorkingDirectory = Global.StartUpPath,
-                    FileName = "BetterGI.exe",
-                    //设置启动动作,确保以管理员身份运行
-                    Verb = "runas"
-                };
-                try
-                {
-                    Process.Start(startInfo);
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine(ex);
-                    return;
-                }
+    /// <summary>
+    /// Occurs when the application is closing.
+    /// </summary>
+    private async void OnExit(object sender, ExitEventArgs e)
+    {
+        await _host.StopAsync();
 
-                //退出
-                Current.Shutdown();
-            }
-        }
+        _host.Dispose();
+    }
 
-        /// <summary>
-        /// Occurs when the application is closing.
-        /// </summary>
-        private async void OnExit(object sender, ExitEventArgs e)
-        {
-            await _host.StopAsync();
-
-            _host.Dispose();
-        }
-
-        /// <summary>
-        /// 注册事件
-        /// </summary>
-        private void RegisterEvents()
-        {
-            //Task线程内未捕获异常处理事件
-            TaskScheduler.UnobservedTaskException += TaskSchedulerUnobservedTaskException;
-
-            //UI线程未捕获异常处理事件（UI主线程）
-            this.DispatcherUnhandledException += AppDispatcherUnhandledException;
-
-            //非UI线程未捕获异常处理事件(例如自己创建的一个子线程)
-            AppDomain.CurrentDomain.UnhandledException += CurrentDomainUnhandledException;
-        }
-
-        private static void TaskSchedulerUnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
-        {
-            try
-            {
-                var exception = e.Exception as Exception;
-                HandleException(exception);
-            }
-            catch (Exception ex)
-            {
-                HandleException(ex);
-            }
-            finally
-            {
-                e.SetObserved();
-            }
-        }
-
-        //非UI线程未捕获异常处理事件(例如自己创建的一个子线程)      
-        private static void CurrentDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
-        {
-            try
-            {
-                var exception = e.ExceptionObject as Exception;
-                if (exception != null)
-                {
-                    HandleException(exception);
-                }
-            }
-            catch (Exception ex)
-            {
-                HandleException(ex);
-            }
-            finally
-            {
-                //ignore
-            }
-        }
+    /// <summary>
+    /// 注册事件
+    /// </summary>
+    private void RegisterEvents()
+    {
+        //Task线程内未捕获异常处理事件
+        TaskScheduler.UnobservedTaskException += TaskSchedulerUnobservedTaskException;
 
         //UI线程未捕获异常处理事件（UI主线程）
-        private static void AppDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        this.DispatcherUnhandledException += AppDispatcherUnhandledException;
+
+        //非UI线程未捕获异常处理事件(例如自己创建的一个子线程)
+        AppDomain.CurrentDomain.UnhandledException += CurrentDomainUnhandledException;
+    }
+
+    private static void TaskSchedulerUnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
+    {
+        try
         {
-            try
+            var exception = e.Exception as Exception;
+            HandleException(exception);
+        }
+        catch (Exception ex)
+        {
+            HandleException(ex);
+        }
+        finally
+        {
+            e.SetObserved();
+        }
+    }
+
+    //非UI线程未捕获异常处理事件(例如自己创建的一个子线程)
+    private static void CurrentDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        try
+        {
+            var exception = e.ExceptionObject as Exception;
+            if (exception != null)
             {
-                HandleException(e.Exception);
-            }
-            catch (Exception ex)
-            {
-                HandleException(ex);
-            }
-            finally
-            {
-                //处理完后，我们需要将Handler=true表示已此异常已处理过
-                e.Handled = true;
+                HandleException(exception);
             }
         }
-
-        private static void HandleException(Exception e)
+        catch (Exception ex)
         {
-            MessageBox.Show("程序异常：" + e.Source + "\r\n--" + Environment.NewLine + e.StackTrace + "\r\n---" + Environment.NewLine + e.Message);
-
-            // log
+            HandleException(ex);
         }
+        finally
+        {
+            //ignore
+        }
+    }
+
+    //UI线程未捕获异常处理事件（UI主线程）
+    private static void AppDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+    {
+        try
+        {
+            HandleException(e.Exception);
+        }
+        catch (Exception ex)
+        {
+            HandleException(ex);
+        }
+        finally
+        {
+            //处理完后，我们需要将Handler=true表示已此异常已处理过
+            e.Handled = true;
+        }
+    }
+
+    private static void HandleException(Exception e)
+    {
+        MessageBox.Show("程序异常：" + e.Source + "\r\n--" + Environment.NewLine + e.StackTrace + "\r\n---" + Environment.NewLine + e.Message);
+
+        // log
     }
 }
---
+++ BetterGenshinImpact/Helpers/UrlProtocolHelper.cs
@@ -0,0 +1,196 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BetterGenshinImpact.Helpers;
+
+internal static class UrlProtocolHelper
+{
+    public const string ProtocolRootKey = @"HKEY_CLASSES_ROOT\";
+    public const string ProtocolUserKey = @"HKEY_CURRENT_USER\" + ProtocolUserSubKey;
+    public const string ProtocolUserSubKey = @"Software\Classes\";
+    public const string ProtocolName = "BetterGI";
+
+    public static RegistryHive RegistryHive { get; set; } = RegistryHive.CurrentUser;
+
+    public static string ProtocolKeyFull => RegistryHive switch
+    {
+        RegistryHive.ClassesRoot => ProtocolRootKey,
+        RegistryHive.CurrentUser or _ => ProtocolUserKey,
+    } + ProtocolName;
+
+    private static RegistryKey? OpenSchemeKey()
+    {
+        if (RegistryHive == RegistryHive.ClassesRoot)
+        {
+            using RegistryKey keyRoot = RegistryKey.OpenBaseKey(RegistryHive.ClassesRoot, RegistryView.Registry64);
+            RegistryKey? keyScheme = keyRoot.OpenSubKey(ProtocolName);
+
+            return keyScheme;
+        }
+        else
+        {
+            using RegistryKey keyUser = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64);
+            using RegistryKey keyUserSub = keyUser.OpenSubKey(ProtocolUserSubKey)!;
+            RegistryKey? keyScheme = keyUserSub.OpenSubKey(ProtocolName);
+
+            return keyScheme;
+        }
+    }
+
+    private static RegistryKey CreateSchemeKey()
+    {
+        if (RegistryHive == RegistryHive.ClassesRoot)
+        {
+            using RegistryKey keyRoot = RegistryKey.OpenBaseKey(RegistryHive.ClassesRoot, RegistryView.Registry64);
+            RegistryKey keyScheme = keyRoot.CreateSubKey(ProtocolName);
+
+            return keyScheme;
+        }
+        else
+        {
+            using RegistryKey keyUser = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64);
+            using RegistryKey keyUserSub = keyUser.OpenSubKey(ProtocolUserSubKey, true)!;
+            RegistryKey keyScheme = keyUserSub.CreateSubKey(ProtocolName);
+
+            return keyScheme;
+        }
+    }
+
+    public static string GetUrl(string param = null!)
+    {
+        return $"{ProtocolName}://{param ?? string.Empty}";
+    }
+
+    public static bool GetPath(out string path)
+    {
+        if (Process.GetCurrentProcess().MainModule?.FileName is string fileName)
+        {
+            path = $"\"{fileName}\" \"%1\"";
+            return true;
+        }
+        path = null!;
+        return false;
+    }
+
+    public static bool IsRegistered()
+    {
+        using RegistryKey? keyScheme = OpenSchemeKey();
+
+        if (keyScheme != null)
+        {
+            using RegistryKey? keyShell = keyScheme.OpenSubKey("shell");
+            using RegistryKey? keyOpen = keyShell?.OpenSubKey("open");
+            using RegistryKey? keyCommand = keyOpen?.OpenSubKey("command");
+
+            if (keyCommand?.GetValue(string.Empty) is string pathRegistered)
+            {
+                if (GetPath(out string path))
+                {
+                    if (pathRegistered == path)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public static string GetVaildPath()
+    {
+        using RegistryKey? keyScheme = OpenSchemeKey();
+
+        if (keyScheme != null)
+        {
+            using RegistryKey? keyShell = keyScheme.OpenSubKey("shell");
+            using RegistryKey? keyOpen = keyShell?.OpenSubKey("open");
+            using RegistryKey? keyCommand = keyOpen?.OpenSubKey("command");
+
+            if (keyCommand?.GetValue(string.Empty) is string pathRegistered)
+            {
+                Regex regex = new("\"(?<exe>[\\s\\S]*?)\" \"%1\"");
+                Match match = regex.Match(pathRegistered);
+
+                if (match.Success && match.Groups["exe"]?.Value is string exe)
+                {
+                    return exe;
+                }
+            }
+        }
+        return null!;
+    }
+
+    public static bool IsVaildProtocol()
+    {
+        return GetVaildPath() != null;
+    }
+
+    public static void Register()
+    {
+        using RegistryKey keyScheme = CreateSchemeKey();
+
+        keyScheme.SetValue(string.Empty, ProtocolName);
+        keyScheme.SetValue("URL Protocol", string.Empty);
+
+        if (GetPath(out string path))
+        {
+            using RegistryKey keyShell = keyScheme.CreateSubKey("shell");
+            using RegistryKey keyOpen = keyShell.CreateSubKey("open");
+            using RegistryKey keyCommand = keyOpen.CreateSubKey("command");
+
+            keyCommand.SetValue(string.Empty, path);
+        }
+    }
+
+    public static void Unregister()
+    {
+        {
+            using RegistryKey keyRoot = RegistryKey.OpenBaseKey(RegistryHive.ClassesRoot, RegistryView.Registry64);
+            using RegistryKey? keyScheme = keyRoot.OpenSubKey(ProtocolName, true);
+
+            if (keyScheme != null)
+            {
+                keyScheme.Close();
+                keyRoot.DeleteSubKeyTree(ProtocolName);
+            }
+        }
+        {
+            using RegistryKey keyUser = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64);
+            using RegistryKey keyUserSub = keyUser.OpenSubKey(ProtocolUserSubKey)!;
+            using RegistryKey? keyScheme = keyUserSub.OpenSubKey(ProtocolName, true);
+
+            if (keyScheme != null)
+            {
+                keyScheme.Close();
+                keyUserSub.DeleteSubKeyTree(ProtocolName);
+            }
+        }
+    }
+
+    public static async Task RegisterAsync()
+    {
+        await Task.Run(() =>
+        {
+            Register();
+        });
+    }
+
+    public static void Launch(string param = null!)
+    {
+        _ = Process.Start(new ProcessStartInfo()
+        {
+            UseShellExecute = true,
+            FileName = GetUrl(param),
+        });
+    }
+
+    public static async Task LaunchAsync(string param = null!)
+    {
+        await Task.Run(() =>
+        {
+            Launch(param);
+        });
+    }
+}
