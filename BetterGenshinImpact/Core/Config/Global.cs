944c61b8679bc4b611df150941e1fafbe496dc98
huiyadanli <huiyadanli@gmail.com>
2023-11-25 23:49:46 +0800

    0.16

--- BetterGenshinImpact/Core/Config/Global.cs
+++ BetterGenshinImpact/Core/Config/Global.cs
@@ -5,7 +5,7 @@ namespace BetterGenshinImpact.Core.Config;
 
 public class Global
 {
-    public static string Version = "0.13.0";
+    public static string Version = "0.16.0";
 
     public static string StartUpPath { get; private set; } = AppContext.BaseDirectory;
 
