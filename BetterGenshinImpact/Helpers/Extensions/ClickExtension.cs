c5aeb197c9357cc29dfcfaec679af6952e91d5e5
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2023-11-26 02:05:06 +0800

    feat:点击模拟添加是否启用随机参数，默认不随机

--- BetterGenshinImpact/Helpers/Extensions/ClickExtension.cs
+++ BetterGenshinImpact/Helpers/Extensions/ClickExtension.cs
@@ -1,21 +1,23 @@
 ﻿using BetterGenshinImpact.Core.Simulator;
 using OpenCvSharp;
+using System;
 using WindowsInput;
 
 namespace BetterGenshinImpact.Helpers.Extensions;
 
 public static class ClickExtension
 {
+    public static Random random = new Random();
     public static void Click(this Point point)
     {
         Simulation.SendInput.Mouse.MoveMouseTo(point.X * 65535 * 1d / PrimaryScreen.WorkingArea.Width,
             point.Y * 65535 * 1d / PrimaryScreen.WorkingArea.Height).LeftButtonDown().Sleep(50).LeftButtonUp();
     }
 
-    public static void ClickCenter(this Rect rect)
+    public static void ClickCenter(this Rect rect, bool isRand=false)
     {
-        Simulation.SendInput.Mouse.MoveMouseTo((rect.X + rect.Width * 1d / 2) * 65535 / PrimaryScreen.WorkingArea.Width,
-            (rect.Y + rect.Height * 1d / 2) * 65535 / PrimaryScreen.WorkingArea.Height).LeftButtonDown().Sleep(50).LeftButtonUp();
+        Simulation.SendInput.Mouse.MoveMouseTo((rect.X + (isRand ? random.Next(rect.Width) : rect.Width * 1d / 2)) * 65535 / PrimaryScreen.WorkingArea.Width,
+            (rect.Y + (isRand ? random.Next(rect.Height) : rect.Height * 1d / 2)) * 65535 / PrimaryScreen.WorkingArea.Height).LeftButtonDown().Sleep(50).LeftButtonUp();
     }
 
     public static IMouseSimulator Click(double x, double y)
