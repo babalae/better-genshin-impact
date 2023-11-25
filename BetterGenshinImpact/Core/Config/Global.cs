8af8b444c22727f325722adff5607632c09f4020
huiyadanli <huiyadanli@gmail.com>
2023-11-25 14:23:52 +0800

    ui copywriting changes

--- BetterGenshinImpact/Core/Config/Global.cs
+++ BetterGenshinImpact/Core/Config/Global.cs
@@ -5,7 +5,7 @@ namespace BetterGenshinImpact.Core.Config;
 
 public class Global
 {
-    public static string Version = "0.15.0";
+    public static string Version = "0.16.0";
 
     public static string StartUpPath { get; private set; } = AppContext.BaseDirectory;
 
