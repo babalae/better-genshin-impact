efd6d1ea410005f63a6ebf222dfb11d8b560e67e
ema <mccoy39082@163.com>
2023-12-17 17:18:45 +0800

    initial script control but debug only

---
+++ BetterGenshinImpact/View/Pages/ScriptControlPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using BetterGenshinImpact.ViewModel.Pages;
+
+namespace BetterGenshinImpact.View.Pages;
+
+public partial class ScriptControlPage
+{
+    public ScriptControlViewModel ViewModel { get; }
+
+    public ScriptControlPage(ScriptControlViewModel viewModel)
+    {
+        DataContext = ViewModel = viewModel;
+        InitializeComponent();
+    }
+}
---
+++ BetterGenshinImpact/ViewModel/Pages/ScriptControlViewModel.cs
@@ -0,0 +1,37 @@
+﻿#if DEBUG
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.ClearScript;
+using Microsoft.ClearScript.V8;
+using Vanara.PInvoke;
+using Wpf.Ui.Controls;
+using System.Diagnostics;
+
+namespace BetterGenshinImpact.ViewModel.Pages;
+
+public partial class ScriptControlViewModel : ObservableObject, INavigationAware
+{
+    public ScriptControlViewModel()
+    {
+        using IScriptEngine engine = new V8ScriptEngine();
+        engine.AddHostObject("lib", new HostTypeCollection("mscorlib", "System.Core"));
+        engine.AddHostObject("win32", new HostTypeCollection("Vanara.PInvoke.User32"));
+        object test = engine.Evaluate("win32.Vanara.PInvoke.User32.GetActiveWindow()");
+        Debug.WriteLine((int)User32.GetActiveWindow());
+        Debug.WriteLine((int)(HWND)test);
+    }
+
+    public void OnNavigatedFrom()
+    {
+    }
+
+    public void OnNavigatedTo()
+    {
+    }
+}
+
+#else
+
+public partial class ScriptControlViewModel : CommunityToolkit.Mvvm.ComponentModel.ObservableObject { }
+
+#endif
