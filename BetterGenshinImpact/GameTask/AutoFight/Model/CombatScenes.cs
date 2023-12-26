f06bddffef9eb3dc1dc703331832459f731bb277
huiyadanli <huiyadanli@gmail.com>
2023-12-26 21:04:44 +0800

    init auto domain

---
+++ BetterGenshinImpact/GameTask/AutoFight/Model/CombatScenes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterGenshinImpact.GameTask.AutoFight.Model;
+
+/// <summary>
+/// 战斗场景
+/// </summary>
+public class CombatScenes
+{
+}
