da87ee0b71f2de32c211ded91c076dadcc906ec7
huiyadanli <huiyadanli@gmail.com>
2024-01-13 17:20:30 +0800

    revert auto skip daily reward press esc

--- BetterGenshinImpact/GameTask/AutoSkip/AutoSkipTrigger.cs
+++ BetterGenshinImpact/GameTask/AutoSkip/AutoSkipTrigger.cs
@@ -137,8 +137,6 @@ public class AutoSkipTrigger : ITaskTrigger
 
                         dailyRewardIconRa.ClickCenter();
                         dailyRewardIconRa.Dispose();
-                        Thread.Sleep(1800);
-                        Simulation.SendInputEx.Keyboard.KeyPress(User32.VK.VK_ESCAPE);
                         return;
                     }
 
