c812681794b8b29c5c2ec52e50fc40c39b322a6b
Mr-Quin <8700123+Mr-Quin@users.noreply.github.com>
2024-03-27 14:23:41 -0700

    Add notification

---
+++ BetterGenshinImpact/View/Pages/NotificationSettingsPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using BetterGenshinImpact.ViewModel.Pages;
+
+namespace BetterGenshinImpact.View.Pages
+{
+    /// <summary>
+    /// NotificationSettingsPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class NotificationSettingsPage : Page
+    {
+        private NotificationSettingsPageViewModel ViewModel { get; }
+
+        public NotificationSettingsPage(NotificationSettingsPageViewModel viewModel)
+        {
+            DataContext = ViewModel = viewModel;
+            InitializeComponent();
+        }
+    }
+}
