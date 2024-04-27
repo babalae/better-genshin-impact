fe039130202624b8d4820c4982296e7c440dff14
辉鸭蛋 <huiyadanli@gmail.com>
2024-04-27 11:48:48 +0800

    add 4.5、4.6 avatar

--- BetterGenshinImpact/GameTask/TaskContext.cs
+++ BetterGenshinImpact/GameTask/TaskContext.cs
@@ -16,9 +16,13 @@ namespace BetterGenshinImpact.GameTask
         private static TaskContext? _uniqueInstance;
         private static object? InstanceLocker;
 
+#pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
+
         private TaskContext()
         {
         }
+
+#pragma warning restore CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
 
         public static TaskContext Instance()
         {
