9878786dd760e8ff4ab5db2c9e68052471dca2b7
辉鸭蛋 <huiyadanli@gmail.com>
2024-07-14 20:18:29 +0800

    init script page

---
+++ BetterGenshinImpact/Model/ScriptGroup.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BetterGenshinImpact.Model;
+
+/// <summary>
+/// 脚本执行组
+/// </summary>
+public partial class ScriptGroup : ObservableObject
+{
+    /// <summary>
+    /// 组名称
+    /// </summary>
+    [ObservableProperty]
+    private string _groupName = string.Empty;
+}
--- BetterGenshinImpact/ViewModel/Pages/ScriptControlViewModel.cs
+++ BetterGenshinImpact/ViewModel/Pages/ScriptControlViewModel.cs
@@ -1,26 +1,40 @@
-﻿using BetterGenshinImpact.ViewModel;
-
-#if DEBUG
-
+﻿using BetterGenshinImpact.Model;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.ClearScript;
 using Microsoft.ClearScript.V8;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using Vanara.PInvoke;
 using Wpf.Ui.Controls;
-using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace BetterGenshinImpact.ViewModel.Pages;
 
 public partial class ScriptControlViewModel : ObservableObject, INavigationAware, IViewModel
 {
+    [ObservableProperty]
+    private ObservableCollection<ScriptGroup> _scriptGroups = [];
+
     public ScriptControlViewModel()
     {
-        using IScriptEngine engine = new V8ScriptEngine();
-        engine.AddHostObject("lib", new HostTypeCollection("mscorlib", "System.Core"));
-        engine.AddHostObject("win32", new HostTypeCollection("Vanara.PInvoke.User32"));
-        object test = engine.Evaluate("win32.Vanara.PInvoke.User32.GetActiveWindow()");
-        Debug.WriteLine((int)User32.GetActiveWindow());
-        Debug.WriteLine((int)(HWND)test);
+        // const V8ScriptEngineFlags flags = V8ScriptEngineFlags.UseCaseInsensitiveMemberBinding;
+        // using IScriptEngine engine = new V8ScriptEngine(flags);
+        // engine.AddHostObject("lib", new HostTypeCollection("mscorlib", "System.Core"));
+        // engine.AddHostObject("win32", new HostTypeCollection("Vanara.PInvoke.User32"));
+        // object test = engine.Evaluate("win32.Vanara.PInvoke.User32.getActiveWindow()");
+        // Debug.WriteLine((int)User32.GetActiveWindow());
+        // Debug.WriteLine((int)(HWND)test);
+
+        //using var runtime = new V8Runtime();
+        //using var engine1 = runtime.CreateScriptEngine();
+        //engine1.Compile("foo = { bar: 123 }");
+        //Debug.WriteLine(engine1.Script.foo);
+
+        // _scriptGroups 加入测试数据
+        for (int i = 0; i < 3; i++)
+        {
+            _scriptGroups.Add(new ScriptGroup { GroupName = $"测试组{i}" });
+        }
     }
 
     public void OnNavigatedFrom()
@@ -31,9 +45,3 @@ public partial class ScriptControlViewModel : ObservableObject, INavigationAware
     {
     }
 }
-
-#else
-
-public partial class ScriptControlViewModel : CommunityToolkit.Mvvm.ComponentModel.ObservableObject, IViewModel { }
-
-#endif
