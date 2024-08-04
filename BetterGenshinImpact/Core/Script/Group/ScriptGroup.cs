d5ac4c523ec1fb7e6144c402d786523a9d89520e
辉鸭蛋 <huiyadanli@gmail.com>
2024-08-04 23:47:54 +0800

    add crud helper

--- BetterGenshinImpact/Core/Script/Group/ScriptGroup.cs
+++ BetterGenshinImpact/Core/Script/Group/ScriptGroup.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace BetterGenshinImpact.Core.Script.Group;
 
@@ -8,5 +9,8 @@ public class ScriptGroup : ObservableObject
     public int Index { get; set; }
     public string Name { get; set; } = string.Empty;
 
+    public bool Enabled { get; set; }
+
+    [JsonIgnore]
     public List<ScriptGroupProject> Projects { get; set; } = [];
 }
