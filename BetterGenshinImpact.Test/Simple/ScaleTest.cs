5726e73d409d30fbe10239f8f2f6d98034f6a540
辉鸭蛋 <huiyadanli@gmail.com>
2024-09-03 00:50:03 +0800

    更新传送点绘制逻辑及界面文本
    在 `MainWindow.xaml` 中，修改了按钮文本，将“超大地图标点”改为“画传送点”，并调整了按钮属性顺序以符合 XAML 标准格式。
    
    在 `MapTeleportPointDraw.cs` 中：
    - 添加命名空间引用。
    - 修改 `Draw` 方法中的文件路径和数据源，增加从 `MapAssets.Instance.TpPositions` 中加载传送点。
    - 修改 `DrawTeleportPoint` 方法参数类型为 `List<GiWorldPosition>`，增加坐标转换和文字标注。
    - 修改 `LoadTeleportPoint` 方法返回类型为 `List<GiWorldPosition>`。
    - 修改 `Transform` 方法返回类型为 `GiWorldPosition`，简化坐标转换逻辑。
    
    在 `ScaleTest.cs` 中：
    - 修改 `ZoomOutTest` 方法加载的图像模式为彩色图。
    - 修改缩小倍数为 4 倍，调整目标文件路径和图像尺寸。

--- BetterGenshinImpact.Test/Simple/ScaleTest.cs
+++ BetterGenshinImpact.Test/Simple/ScaleTest.cs
@@ -6,11 +6,11 @@ public class ScaleTest
 {
     public static void ZoomOutTest()
     {
-        var mainMap2048BlockMat = new Mat(@"E:\HuiTask\更好的原神\地图匹配\有用的素材\5.0\mainMap2048Block.png", ImreadModes.Grayscale);
-        // 缩小 2048/256 = 8 倍
-        var targetFilePath = @"E:\HuiTask\更好的原神\地图匹配\有用的素材\5.0\mainMap256Block.png";
+        var mainMap2048BlockMat = new Mat(@"E:\HuiTask\更好的原神\地图匹配\有用的素材\5.0\mainMap2048Block.png", ImreadModes.Color);
+        // 缩小 2048/512 = 4
+        var targetFilePath = @"E:\HuiTask\更好的原神\地图匹配\有用的素材\5.0\mainMap1024BlockColor.png";
         // opencv 缩小
-        var mainMap256BlockMat = mainMap2048BlockMat.Resize(new Size(mainMap2048BlockMat.Width / 8, mainMap2048BlockMat.Height / 8));
+        var mainMap256BlockMat = mainMap2048BlockMat.Resize(new Size(mainMap2048BlockMat.Width / 2, mainMap2048BlockMat.Height / 2));
         mainMap256BlockMat.SaveImage(targetFilePath);
     }
 }
