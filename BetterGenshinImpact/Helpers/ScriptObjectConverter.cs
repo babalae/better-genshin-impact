aee54cc11562c8a46ba5ca25d63bb9ea27180432
辉鸭蛋 <huiyadanli@gmail.com>
2024-10-01 20:25:39 +0800

    js config

--- BetterGenshinImpact/Helpers/ScriptObjectConverter.cs
+++ BetterGenshinImpact/Helpers/ScriptObjectConverter.cs
@@ -13,23 +13,30 @@ public class ScriptObjectConverter
 
         foreach (PropertyInfo property in type.GetProperties())
         {
-            if (source[property.Name] is not Undefined && source[property.Name] != null)
-            {
-                object value = source.GetProperty(property.Name);
-
-                if (property.PropertyType.IsPrimitive || property.PropertyType == typeof(string))
-                {
-                    property.SetValue(result, Convert.ChangeType(value, property.PropertyType));
-                }
-                else
-                {
-                    MethodInfo method = typeof(ScriptObjectConverter).GetMethod("ConvertTo")!.MakeGenericMethod(property.PropertyType);
-                    object? nestedValue = method.Invoke(null, [value]);
-                    property.SetValue(result, nestedValue);
-                }
-            }
+            // 支持 PascalCase 和 camelCase
+            DealWithProperty(source, property, char.ToLower(property.Name[0]) + property.Name[1..], result);
+            DealWithProperty(source, property, char.ToUpper(property.Name[0]) + property.Name[1..], result);
         }
 
         return result;
+    }
+
+    private static void DealWithProperty<T>(ScriptObject source, PropertyInfo property, string propertyName, T result)
+    {
+        if (source[propertyName] is not Undefined && source[propertyName] != null)
+        {
+            object value = source.GetProperty(propertyName);
+
+            if (property.PropertyType.IsPrimitive || property.PropertyType == typeof(string))
+            {
+                property.SetValue(result, Convert.ChangeType(value, property.PropertyType));
+            }
+            else
+            {
+                MethodInfo method = typeof(ScriptObjectConverter).GetMethod("ConvertTo")!.MakeGenericMethod(property.PropertyType);
+                object? nestedValue = method.Invoke(null, [value]);
+                property.SetValue(result, nestedValue);
+            }
+        }
     }
 }
