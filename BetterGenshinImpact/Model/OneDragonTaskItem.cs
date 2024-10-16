9ad106202072d3aa26878f84d51f69ef4204337d
辉鸭蛋 <huiyadanli@gmail.com>
2024-10-17 03:25:25 +0800

    update one-stop service ui

---
+++ BetterGenshinImpact/Model/OneDragonTaskItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BetterGenshinImpact.Model;
+
+public partial class OneDragonTaskItem : ObservableObject
+{
+    [ObservableProperty]
+    private string? _name;
+
+    [ObservableProperty]
+    private Brush? _statusColor;
+
+    [ObservableProperty]
+    private bool _isEnabled;
+}
--- BetterGenshinImpact/ViewModel/Pages/OneDragonFlowViewModel.cs
+++ BetterGenshinImpact/ViewModel/Pages/OneDragonFlowViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Logging;
+using System.Collections.ObjectModel;
+using System.Windows.Media;
+using BetterGenshinImpact.Model;
 using Wpf.Ui;
 using Wpf.Ui.Controls;
 
@@ -11,6 +14,12 @@ public partial class OneDragonFlowViewModel : ObservableObject, INavigationAware
     private readonly ILogger<OneDragonFlowViewModel> _logger = App.GetLogger<OneDragonFlowViewModel>();
 
     private ISnackbarService _snackbarService;
+
+    [ObservableProperty]
+    private ObservableCollection<OneDragonTaskItem> _taskList = [
+        new OneDragonTaskItem { Name = "登录游戏", StatusColor = Brushes.Gray, IsEnabled = true },
+        new OneDragonTaskItem { Name = "合成树脂", StatusColor = Brushes.Gray, IsEnabled = true },
+    ];
 
     public OneDragonFlowViewModel(ISnackbarService snackbarService)
     {
