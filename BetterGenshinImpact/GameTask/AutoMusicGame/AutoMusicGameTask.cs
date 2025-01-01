3c393087ebcd3614d1de9dd10ab9308fa70d50a5
辉鸭蛋 <huiyadanli@gmail.com>
2025-01-01 19:34:39 +0800

    auto music opt

--- BetterGenshinImpact/GameTask/AutoMusicGame/AutoMusicGameTask.cs
+++ BetterGenshinImpact/GameTask/AutoMusicGame/AutoMusicGameTask.cs
@@ -30,26 +30,32 @@ public class AutoMusicGameTask(AutoMusicGameParam taskParam) : ISoloTask
 
     private readonly IntPtr _hWnd = TaskContext.Instance().GameHandle;
 
-    public Task Start(CancellationToken ct)
+    public async Task Start(CancellationToken ct)
     {
-        Init();
-
-        var assetScale = TaskContext.Instance().SystemInfo.AssetScale;
-        var taskFactory = new TaskFactory();
-        var taskList = new List<Task>();
-
-        // 计算按键位置
-        var gameCaptureRegion = CaptureToRectArea();
-
-        foreach (var keyValuePair in _keyX)
+        try
         {
-            var (x, y) = gameCaptureRegion.ConvertPositionToGameCaptureRegion((int)(keyValuePair.Value * assetScale), (int)(_keyY * assetScale));
-            // 添加任务
-            taskList.Add(taskFactory.StartNew(async () => await DoWhitePressWin32(ct, keyValuePair.Key, new Point(x, y)), ct));
-        }
+            Init();
 
-        Task.WaitAll([.. taskList], ct);
-        return Task.CompletedTask;
+            var assetScale = TaskContext.Instance().SystemInfo.AssetScale;
+            // var taskFactory = new TaskFactory();
+            var taskList = new List<Task>();
+
+            // 计算按键位置
+            var gameCaptureRegion = CaptureToRectArea();
+
+            foreach (var keyValuePair in _keyX)
+            {
+                var (x, y) = gameCaptureRegion.ConvertPositionToGameCaptureRegion((int)(keyValuePair.Value * assetScale), (int)(_keyY * assetScale));
+                // 添加任务
+                taskList.Add(Task.Run(async () => await DoWhitePressWin32(ct, keyValuePair.Key, new Point(x, y)), ct));
+            }
+
+            await Task.WhenAll(taskList);
+        }
+        finally
+        {
+            Simulation.ReleaseAllKey();
+        }
     }
 
     private async Task DoWhitePressWin32(CancellationToken ct, User32.VK key, Point point)
@@ -106,7 +112,9 @@ public class AutoMusicGameTask(AutoMusicGameParam taskParam) : ISoloTask
         var gameScreenSize = SystemControl.GetGameScreenRect(TaskContext.Instance().GameHandle);
         if (gameScreenSize.Width * 9 != gameScreenSize.Height * 16)
         {
-            Logger.LogWarning("游戏窗口分辨率不是 16:9 ！当前分辨率为 {Width}x{Height} , 非 16:9 分辨率的游戏可能无法正常使用自动活动音游功能 !", gameScreenSize.Width, gameScreenSize.Height);
+            Logger.LogError("游戏窗口分辨率不是 16:9 ！当前分辨率为 {Width}x{Height} , 非 16:9 分辨率的游戏无法正常使用自动活动音游功能 !", gameScreenSize.Width, gameScreenSize.Height);
+            throw new Exception("游戏窗口分辨率不是 16:9");
         }
+        Logger.LogInformation("回到游戏主界面时记得关闭自动音游任务！");
     }
 }
