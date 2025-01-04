4cfff57b2c85e9f04f3b2d6f95dce67dbe46c350
Ayu0K <YZJ123abc@outlook.com>
2025-01-04 15:03:29 +0800

    mod: 修改AutoFightTask中检测战斗结束使用的打开编队界面和取消攀爬的按键为KeyBindingsConfig记录的按键；修改方案参考AutoWood，在调用Start方法时将该模块需要用到的按键从KeyBindingsConfig中取出

--- BetterGenshinImpact/GameTask/AutoFight/AutoFightTask.cs
+++ BetterGenshinImpact/GameTask/AutoFight/AutoFightTask.cs
@@ -37,6 +37,10 @@ public class AutoFightTask : ISoloTask
 
     private readonly double _dpi = TaskContext.Instance().DpiScale;
 
+    private User32.VK _partySetupKey = User32.VK.VK_L;
+
+    private User32.VK _dropKey = User32.VK.VK_X;
+
 
     private class TaskFightFinishDetectConfig
     {
@@ -185,6 +189,11 @@ public class AutoFightTask : ISoloTask
     public List<string> checkNames = new();*/
     public async Task Start(CancellationToken ct)
     {
+        // 从KeyBindingsConfig中读取配置
+        var keyConfig = TaskContext.Instance().Config.KeyBindingsConfig;
+        _partySetupKey = keyConfig.OpenPartySetupScreen;
+        _dropKey = keyConfig.Drop;
+
         _ct = ct;
 
         LogScreenResolution();
@@ -418,9 +427,9 @@ public class AutoFightTask : ISoloTask
         **/
   
         await Delay(delayTime, _ct);
-        Logger.LogInformation("按L检查战斗是否结束");
+        Logger.LogInformation("打开编队界面检查战斗是否结束");
         // 最终方案确认战斗结束
-        Simulation.SendInput.Keyboard.KeyPress(User32.VK.VK_L);
+        Simulation.SendInput.Keyboard.KeyPress(_partySetupKey);
         await Delay(450, _ct);
         var ra = CaptureToRectArea();
         var b3 = ra.SrcMat.At<Vec3b>(50, 790);
@@ -428,11 +437,11 @@ public class AutoFightTask : ISoloTask
         if (AreDifferencesWithinBounds(_finishDetectConfig.BattleEndProgressBarColor, (b3.Item0, b3.Item1, b3.Item2), _finishDetectConfig.BattleEndProgressBarColorTolerance))
         {
             Logger.LogInformation("识别到战斗结束");
-            Simulation.SendInput.Keyboard.KeyPress(User32.VK.VK_X);
+            Simulation.SendInput.Keyboard.KeyPress(_dropKey);
             return true;
         }
 
-        Simulation.SendInput.Keyboard.KeyPress(User32.VK.VK_X);
+        Simulation.SendInput.Keyboard.KeyPress(_dropKey);
         Logger.LogInformation($"未识别到战斗结束{b3.Item0},{b3.Item1},{b3.Item2}");
         _lastFightFlagTime = DateTime.Now;
         return false;
