58d79d5955c79babc3b1ca6e8ed6b33d3fd24822
辉鸭蛋 <huiyadanli@gmail.com>
2025-01-19 19:35:16 +0800

    Revert "测试代码"
    This reverts commit d1fcb9c21d7836fb1a01307ea61904adf4c6aa74.

--- BetterGenshinImpact/View/Pages/HomePage.xaml.cs
+++ BetterGenshinImpact/View/Pages/HomePage.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using BetterGenshinImpact.ViewModel.Pages;
-using Vanara.PInvoke;
+﻿using BetterGenshinImpact.ViewModel.Pages;
 
 namespace BetterGenshinImpact.View.Pages
 {
@@ -15,27 +12,6 @@ namespace BetterGenshinImpact.View.Pages
             InitializeComponent();
 
             // hotKeyPageViewModel 放在这里是为了在首页就初始化热键
-
-            Task.Run(async () =>
-            {
-                bool first = true;
-                // 显示当前时间
-                while (true)
-                {
-                    Dispatcher.Invoke(() =>
-                    {
-                        if (first)
-                        {
-                            first = false;
-                            ClockStartBlock.Text = DateTime.Now.ToString("HH:mm:ss.fff") + " | " + Kernel32.GetTickCount().ToString();
-                        }
-
-                        ClockBlock.Text = DateTime.Now.ToString("HH:mm:ss.fff");
-                        TickBlock.Text = Kernel32.GetTickCount().ToString();
-                    });
-                    await Task.Delay(10);
-                }
-            });
         }
     }
 }
