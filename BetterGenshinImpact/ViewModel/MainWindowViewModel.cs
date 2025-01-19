ca96204e012fe03035f138adaf3726148c4a3bd7
辉鸭蛋 <huiyadanli@gmail.com>
2025-01-20 00:22:40 +0800

    install path

--- BetterGenshinImpact/ViewModel/MainWindowViewModel.cs
+++ BetterGenshinImpact/ViewModel/MainWindowViewModel.cs
@@ -15,6 +15,7 @@ using System;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -39,7 +40,7 @@ public partial class MainWindowViewModel : ObservableObject, IViewModel
 
     [ObservableProperty]
     public WindowBackdropType _currentBackdropType = WindowBackdropType.Auto;
-    
+
     public AllConfig Config { get; set; }
 
     public MainWindowViewModel(INavigationService navigationService, IConfigService configService)
@@ -60,23 +61,24 @@ public partial class MainWindowViewModel : ObservableObject, IViewModel
     {
         IsVisible = false;
     }
+
     [RelayCommand]
     private void OnSwitchBackdrop()
     {
         CurrentBackdropType = CurrentBackdropType switch
         {
-            WindowBackdropType.Auto => WindowBackdropType.Mica,
             WindowBackdropType.Mica => WindowBackdropType.Acrylic,
             WindowBackdropType.Acrylic => WindowBackdropType.Tabbed,
-            WindowBackdropType.Tabbed => WindowBackdropType.Auto,
-            _ => WindowBackdropType.Auto
+            WindowBackdropType.Tabbed => WindowBackdropType.Mica,
+            _ => WindowBackdropType.Mica
         };
 
         Config.CommonConfig.CurrentBackdropType = CurrentBackdropType;
 
         if (Application.Current.MainWindow is MainWindow mainWindow)
         {
-            mainWindow.Background = new SolidColorBrush(Color.FromArgb(100, 0, 0, 0));;
+            mainWindow.Background = new SolidColorBrush(Color.FromArgb(100, 0, 0, 0));
+            ;
             WindowBackdrop.ApplyBackdrop(mainWindow, CurrentBackdropType);
         }
     }
@@ -92,8 +94,53 @@ public partial class MainWindowViewModel : ObservableObject, IViewModel
     }
 
     [RelayCommand]
-    [SuppressMessage("CommunityToolkit.Mvvm.SourceGenerators.RelayCommandGenerator", "MVVMTK0039:Async void returning method annotated with RelayCommand")]
-    private async void OnLoaded()
+    private async Task OnLoaded()
+    {
+        // 自动处理目录配置
+        await Patch1();
+
+        // 预热OCR
+        await OcrPreheating();
+
+        // 检查更新
+        await App.GetService<IUpdateService>()!.CheckUpdateAsync(new UpdateOption());
+
+        //  Win11下 BitBlt截图方式不可用，需要关闭窗口优化功能
+        if (OsVersionHelper.IsWindows11_OrGreater && TaskContext.Instance().Config.AutoFixWin11BitBlt)
+        {
+            BitBltRegistryHelper.SetDirectXUserGlobalSettings();
+        }
+
+        // 更新仓库
+        ScriptRepoUpdater.Instance.AutoUpdate();
+    }
+
+
+    /**
+     * 不同的安装目录处理
+     * 可能当前目录下存在 BetterGI 的文件，需要移动到新的目录
+     */
+    private async Task Patch1()
+    {
+        if (Directory.Exists(Global.Absolute("BetterGI")) 
+            // && File.Exists(Global.Absolute("BetterGI/BetterGI.exe"))
+            && Directory.Exists(Global.Absolute("BetterGI/User"))
+            )
+        {
+            var res = await MessageBox.ShowAsync("检测到旧的 BetterGI 配置，是否迁移配置并清理旧目录？", "BetterGI", System.Windows.MessageBoxButton.YesNo);
+            if (res == System.Windows.MessageBoxResult.Yes)
+            {
+                var dir = Global.Absolute("BetterGI/User");
+                // 迁移配置，拷贝整个目录并覆盖
+                DirectoryHelper.CopyDirectory(dir, Global.Absolute("User"));
+                // 删除旧目录
+                Directory.Delete(Global.Absolute("BetterGI"));
+            }
+
+        }
+    }
+
+    private async Task OcrPreheating()
     {
         try
         {
@@ -125,16 +172,5 @@ public partial class MainWindowViewModel : ObservableObject, IViewModel
         {
             MessageBox.Warning("PaddleOcr预热失败，解决方案：https://bgi.huiyadan.com/faq.html，" + e.Source + "\r\n--" + Environment.NewLine + e.StackTrace + "\r\n---" + Environment.NewLine + e.Message);
         }
-
-        await App.GetService<IUpdateService>()!.CheckUpdateAsync(new UpdateOption());
-
-        //  Win11下 BitBlt截图方式不可用，需要关闭窗口优化功能
-        if (OsVersionHelper.IsWindows11_OrGreater && TaskContext.Instance().Config.AutoFixWin11BitBlt)
-        {
-            BitBltRegistryHelper.SetDirectXUserGlobalSettings();
-        }
-
-        // 更新仓库
-        ScriptRepoUpdater.Instance.AutoUpdate();
     }
 }
