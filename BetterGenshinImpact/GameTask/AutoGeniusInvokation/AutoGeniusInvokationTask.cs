3c84f3817d9e59d29fd6afb5fd2fe302bfb05979
辉鸭蛋 <huiyadanli@gmail.com>
2025-01-24 22:48:25 +0800

    auto tcg: fix error

--- BetterGenshinImpact/GameTask/AutoGeniusInvokation/AutoGeniusInvokationTask.cs
+++ BetterGenshinImpact/GameTask/AutoGeniusInvokation/AutoGeniusInvokationTask.cs
@@ -1,5 +1,7 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using BetterGenshinImpact.GameTask.Common;
+using Microsoft.Extensions.Logging;
 
 namespace BetterGenshinImpact.GameTask.AutoGeniusInvokation;
 
@@ -9,9 +11,17 @@ public class AutoGeniusInvokationTask(GeniusInvokationTaskParam taskParam) : ISo
 
     public Task Start(CancellationToken ct)
     {
-        // 读取策略信息
-        var duel = ScriptParser.Parse(taskParam.StrategyContent);
-        duel.Run(ct);
+        try
+        {
+            // 读取策略信息
+            var duel = ScriptParser.Parse(taskParam.StrategyContent);
+            duel.Run(ct);
+        }
+        catch (System.Exception e)
+        {
+            TaskControl.Logger.LogDebug(e, "执行自动七圣召唤任务异常");
+            TaskControl.Logger.LogError("执行自动七圣召唤任务异常：{Exception}", e.Message);
+        }
         return Task.CompletedTask;
     }
 }
