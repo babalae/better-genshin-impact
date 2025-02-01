1eee073adb11de89662674e00bc8409a9b208200
辉鸭蛋 <huiyadanli@gmail.com>
2025-02-01 18:11:51 +0800

    1. 修正 Orientation 大写的问题 2. 美化了是否作为 target 点位接近的

--- BetterGenshinImpact/GameTask/AutoPathing/PathExecutor.cs
+++ BetterGenshinImpact/GameTask/AutoPathing/PathExecutor.cs
@@ -178,32 +178,21 @@ public class PathExecutor
                         {
                             await BeforeMoveToTarget(waypoint);
 
+                            // Path不用走得很近，Target需要接近，但都需要先移动到对应位置
                             if (waypoint.Type == WaypointType.Orientation.Code)
+                            {
                                 // 方位点，只需要朝向
+                                // 考虑到方位点大概率是作为执行action的最后一个点，所以放在此处处理，不和传送点一样单独处理
                                 await FaceTo(waypoint);
+                            }
                             else
-                                // Path不用走得很近，Target需要接近，但都需要先移动到对应位置
+                            {
                                 await MoveTo(waypoint);
+                            }
 
                             await BeforeMoveCloseToTarget(waypoint);
 
-                            if (
-                                waypoint.Type != WaypointType.Orientation.Code // 方位点不需要接近
-                                && waypoint.Action != ActionEnum.Fight.Code // 战斗action强制不接近
-                                && (
-                                        waypoint.Type == WaypointType.Target.Code
-                                        || (
-                                                !string.IsNullOrEmpty(waypoint.Action)
-                                                && waypoint.Action != ActionEnum.StopFlying.Code
-                                                && waypoint.Action != ActionEnum.NahidaCollect.Code
-                                                && waypoint.Action != ActionEnum.CombatScript.Code
-                                                && waypoint.Action != ActionEnum.Mining.Code
-                                            // 除了上述之外的actions都强制接近，即时类型是Path（真的需要这样吗？）
-                                            // force_tp, up_down_grab_leaf, log_output, 这几个不需要接近；
-                                            // pick_around, hydro_collect, electro_collect, anemo_collect, 这几个现存脚本中没有用target？
-                                            )
-                                    )
-                                )
+                            if (IsTargetPoint(waypoint))
                             {
                                 await MoveCloseTo(waypoint);
                             }
@@ -262,6 +251,25 @@ public class PathExecutor
                 }
             }
         }
+    }
+
+    private bool IsTargetPoint(WaypointForTrack waypoint)
+    {
+        // 方位点不需要接近
+        if (waypoint.Type == WaypointType.Orientation.Code)
+        {
+            return false;
+        }
+        
+        var action = ActionEnum.GetEnumByCode(waypoint.Action);
+        if (action is not null && action.UseWaypointTypeEnum != ActionUseWaypointTypeEnum.Custom)
+        {
+            // 强制点位类型的 action，以 action 为准
+            return action.UseWaypointTypeEnum == ActionUseWaypointTypeEnum.Target;
+        }
+
+        // 其余情况和没有action的情况以点位类型为准
+        return waypoint.Type == WaypointType.Target.Code;
     }
 
     private async Task<bool> SwitchPartyBefore(PathingTask task)
