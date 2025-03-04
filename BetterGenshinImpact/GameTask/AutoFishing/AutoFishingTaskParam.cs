e634a54913d46cc32e503500c794e1da16ab469f
FishmanTheMurloc <checkitout@yeah.net>
2025-03-04 21:06:16 +0800

    钓鱼任务添加整个任务的超时参数、昼夜参数

---
+++ BetterGenshinImpact/GameTask/AutoFishing/AutoFishingTaskParam.cs
@@ -0,0 +1,21 @@
+﻿using BetterGenshinImpact.GameTask.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BetterGenshinImpact.GameTask.AutoFishing
+{
+    public class AutoFishingTaskParam : BaseTaskParam
+    {
+        public AutoFishingTaskParam(int wholeProcessTimeoutSeconds, int throwRodTimeOutTimeoutSeconds, FishingTimePolicy fishingTimePolicy)
+        {
+            WholeProcessTimeoutSeconds = wholeProcessTimeoutSeconds;
+            ThrowRodTimeOutTimeoutSeconds = throwRodTimeOutTimeoutSeconds;
+            FishingTimePolicy = fishingTimePolicy;
+        }
+
+        public int WholeProcessTimeoutSeconds { get; set; }
+        public int ThrowRodTimeOutTimeoutSeconds { get; set; }
+        public FishingTimePolicy FishingTimePolicy { get; set; }
+    }
+}
---
+++ BetterGenshinImpact/GameTask/AutoFishing/FishingTimePolicy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BetterGenshinImpact.GameTask.AutoFishing
+{
+    public enum FishingTimePolicy
+    {
+        [Description("全天")]
+        All,
+        [Description("白天")]
+        Daytime,
+        [Description("夜晚")]
+        Nighttime
+    }
+}
