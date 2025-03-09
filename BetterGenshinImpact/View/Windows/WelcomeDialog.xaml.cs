366e5d140a61b53bf8a9211331918b62873380f4
辉鸭蛋 <huiyadanli@gmail.com>
2025-03-09 18:55:25 +0800

    add WelcomeDialog

---
+++ BetterGenshinImpact/View/Windows/WelcomeDialog.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Windows.System;
+
+namespace BetterGenshinImpact.View.Windows;
+
+public partial class WelcomeDialog
+{
+    public WelcomeDialog()
+    {
+        InitializeComponent();
+        this.Loaded += WelcomeDialogLoaded;
+    }
+
+    private void WelcomeDialogLoaded(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    public static void Prompt(string question, string title, string defaultValue = "")
+    {
+        var inst = new WelcomeDialog();
+        inst.ShowDialog();
+    }
+
+    private void BtnOkClick(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;
+        Close();
+    }
+
+    private void HyperlinkRequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+        string uri = e.Uri.AbsoluteUri;
+        Launcher.LaunchUriAsync(new Uri(uri)).Wait();
+    }
+}
