46ebdfc4527ec311781bb7fa20b11fdfa278aeee
辉鸭蛋 <huiyadanli@gmail.com>
2025-03-13 00:33:31 +0800

    wpfui 4.0.0 (#1255)
    * new INavigationAware, remove PageService
    
    * fix run
    
    * fix title bar
    
    * override OnNavigatedTo
    
    * try fix WindowStartupLocation

--- BetterGenshinImpact/View/MainWindow.xaml.cs
+++ BetterGenshinImpact/View/MainWindow.xaml.cs
@@ -7,18 +7,19 @@ using System.Windows.Media;
 using BetterGenshinImpact.GameTask;
 using BetterGenshinImpact.Helpers.Ui;
 using Wpf.Ui;
+using Wpf.Ui.Abstractions;
 using Wpf.Ui.Controls;
 using Wpf.Ui.Tray.Controls;
 
 namespace BetterGenshinImpact.View;
 
-public partial class MainWindow : FluentWindow, INavigationWindow
+public partial class MainWindow : INavigationWindow
 {
     private readonly ILogger<MainWindow> _logger = App.GetLogger<MainWindow>();
 
     public MainWindowViewModel ViewModel { get; }
 
-    public MainWindow(MainWindowViewModel viewModel, IPageService pageService, INavigationService navigationService, ISnackbarService snackbarService)
+    public MainWindow(MainWindowViewModel viewModel, INavigationService navigationService, ISnackbarService snackbarService)
     {
         _logger.LogDebug("主窗体实例化");
         DataContext = ViewModel = viewModel;
@@ -26,7 +27,6 @@ public partial class MainWindow : FluentWindow, INavigationWindow
         InitializeComponent();
         this.InitializeDpiAwareness();
 
-        SetPageService(pageService);
         snackbarService.SetSnackbarPresenter(SnackbarPresenter);
         navigationService.SetNavigationControl(RootNavigation);
 
@@ -62,10 +62,8 @@ public partial class MainWindow : FluentWindow, INavigationWindow
         throw new NotImplementedException();
     }
 
-    public void SetPageService(IPageService pageService)
-    {
-        RootNavigation.SetPageService(pageService);
-    }
+    public void SetPageService(INavigationViewPageProvider navigationViewPageProvider) =>
+        RootNavigation.SetPageProviderService(navigationViewPageProvider);
 
     public void ShowWindow() => Show();
 
