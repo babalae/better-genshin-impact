55a7dec210cc723c9981567c3ef30f60c9a88153
辉鸭蛋 <huiyadanli@gmail.com>
2025-03-26 22:36:39 +0800

    Add About option to settings page (#1348)
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/babalae/better-genshin-impact?shareId=XXXX-XXXX-XXXX-XXXX).

---
+++ BetterGenshinImpact/View/Windows/AboutWindow.xaml.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace BetterGenshinImpact.View.Windows
+{
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            e.Handled = true;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
