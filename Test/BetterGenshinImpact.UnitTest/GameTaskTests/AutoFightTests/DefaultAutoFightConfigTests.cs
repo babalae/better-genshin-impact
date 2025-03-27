8b75eef52f6cb66702d198e4fcfd7826d1e4babe
FishmanTheMurloc <checkitout@yeah.net>
2025-03-27 22:09:19 +0800

    修复有3个Mizuki导致异常的bug，临时用拼音代替新角色英文名，并为该数据初始化方法添加单元测试

---
+++ Test/BetterGenshinImpact.UnitTest/GameTaskTests/AutoFightTests/DefaultAutoFightConfigTests.cs
@@ -0,0 +1,28 @@
+﻿using BetterGenshinImpact.GameTask.AutoFight.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterGenshinImpact.UnitTest.GameTaskTests.AutoFightTests
+{
+    public partial class DefaultAutoFightConfigTests
+    {
+        /// <summary>
+        /// 测试默认战斗配置初始化，结果应成功
+        /// </summary>
+        [Fact]
+        public void DefaultAutoFightConfig_Init_ShouldSuccess()
+        {
+            //
+            // Do nothing
+
+            //
+            var sut = DefaultAutoFightConfig.CombatAvatars;
+
+            //
+            Assert.NotNull(sut);
+        }
+    }
+}
