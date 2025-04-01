f23975f04c1d39948c453ee16ace45f491e2edde
辉鸭蛋 <huiyadanli@gmail.com>
2025-04-02 00:43:25 +0800

    地图追踪：没有按下前进键时，按下前进键

--- BetterGenshinImpact/GameTask/AutoPathing/PathExecutor.cs
+++ BetterGenshinImpact/GameTask/AutoPathing/PathExecutor.cs
@@ -629,6 +629,11 @@ public class PathExecutor
         Simulation.SendInput.SimulateAction(GIActions.MoveForward, KeyType.KeyDown);
         while (!ct.IsCancellationRequested)
         {
+            if (!Simulation.IsKeyDown(GIActions.MoveForward.ToActionKey().ToVK()))
+            {
+                Simulation.SendInput.SimulateAction(GIActions.MoveForward, KeyType.KeyDown);
+            }
+            
             num++;
             if ((DateTime.UtcNow - moveToStartTime).TotalSeconds > 240)
             {
