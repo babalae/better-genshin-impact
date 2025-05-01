ee50147e0eb07468a4896193f98aa58f8f32cabd
辉鸭蛋 <huiyadanli@gmail.com>
2025-05-02 00:29:06 +0800

    添加远古圣山,修改地图参数

--- BetterGenshinImpact/GameTask/Common/Map/Maps/SeaOfBygoneErasMap.cs
+++ BetterGenshinImpact/GameTask/Common/Map/Maps/SeaOfBygoneErasMap.cs
@@ -5,15 +5,17 @@ namespace BetterGenshinImpact.GameTask.Common.Map.Maps;
 
 /// <summary>
 /// 旧日之海
+/// 从3x4改成了1x2
+/// 大地图都是半黑的,传送可能有问题
 /// </summary>
 public class SeaOfBygoneErasMap : IndependentBaseMap
 {
     #region 地图参数
 
-    static readonly int GameMapRows = 3; // 游戏坐标下地图块的行数
-    static readonly int GameMapCols = 4; // 游戏坐标下地图块的列数
-    static readonly int GameMapUpRows = 1; // 游戏坐标下 左上角离地图原点的行数(注意原点在块的右下角)
-    static readonly int GameMapLeftCols = 1; // 游戏坐标下 左上角离地图原点的列数(注意原点在块的右下角)
+    static readonly int GameMapRows = 1; // 游戏坐标下地图块的行数
+    static readonly int GameMapCols = 2; // 游戏坐标下地图块的列数
+    static readonly int GameMapUpRows = 0; // 游戏坐标下 左上角离地图原点的行数(注意原点在块的右下角)  TODO 没找到
+    static readonly int GameMapLeftCols = 0; // 游戏坐标下 左上角离地图原点的列数(注意原点在块的右下角)  TODO 没找到
 
     #endregion 地图参数
 
--- BetterGenshinImpact/GameTask/Common/Map/Maps/TheChasmMap.cs
+++ BetterGenshinImpact/GameTask/Common/Map/Maps/TheChasmMap.cs
@@ -5,15 +5,16 @@ namespace BetterGenshinImpact.GameTask.Common.Map.Maps;
 
 /// <summary>
 /// 层岩巨渊
+/// 地图大小从3x3改到了2x2
 /// </summary>
 public class TheChasmMap : IndependentBaseMap
 {
     #region 每次地图扩大都要更新的参数(层岩巨渊无需更新)
 
-    static readonly int GameMapRows = 3; // 游戏坐标下地图块的行数
-    static readonly int GameMapCols = 3; // 游戏坐标下地图块的列数
-    static readonly int GameMapUpRows = 1; // 游戏坐标下 左上角离地图原点的行数(注意原点在块的右下角)
-    static readonly int GameMapLeftCols = 1; // 游戏坐标下 左上角离地图原点的列数(注意原点在块的右下角)
+    static readonly int GameMapRows = 2; // 游戏坐标下地图块的行数
+    static readonly int GameMapCols = 2; // 游戏坐标下地图块的列数
+    static readonly int GameMapUpRows = 1; // 游戏坐标下 左上角离地图原点的行数(注意原点在0_0块的右下角)
+    static readonly int GameMapLeftCols = 1; // 游戏坐标下 左上角离地图原点的列数(注意原点在0_0块的右下角)
 
     #endregion 每次地图扩大都要更新的参数(层岩巨渊无需更新)
 
