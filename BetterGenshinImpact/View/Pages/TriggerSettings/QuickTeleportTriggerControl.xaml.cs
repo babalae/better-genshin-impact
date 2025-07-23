d723a7dbfd797f7fd287d1d5f6d8e9d081d5f885
DR-lin-eng <@DR-lin-eng>
2025-07-23 19:22:43 +0800

    重构实时任务代码，提升可维护性。完成99%的本地化工作

---
+++ BetterGenshinImpact/View/Pages/TriggerSettings/QuickTeleportTriggerControl.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace BetterGenshinImpact.View.Pages.TriggerSettings
+{
+    /// <summary>
+    /// QuickTeleportTriggerControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class QuickTeleportTriggerControl : UserControl
+    {
+        public QuickTeleportTriggerControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
