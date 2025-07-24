63d0b0191ff96c7361b5df566fbf6254bb1bee8c
DR-lin-eng <52230594+DR-lin-eng@users.noreply.github.com>
2025-07-24 17:47:41 +0800

    Add files via upload
    解决编码错误

---
+++ BetterGenshinImpact/GameTask/TaskRunner.cs
@@ -0,0 +1,156 @@
+﻿using BetterGenshinImpact.Core.Script;
+using BetterGenshinImpact.GameTask.AutoGeniusInvokation.Exception;
+
+using BetterGenshinImpact.View;
+using BetterGenshinImpact.View.Drawable;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BetterGenshinImpact.Helpers;
+using Wpf.Ui.Violeta.Controls;
+using static BetterGenshinImpact.GameTask.Common.TaskControl;
+using BetterGenshinImpact.Service;
+using BetterGenshinImpact.Service.Notification;
+using BetterGenshinImpact.Service.Notification.Model.Enum;
+
+namespace BetterGenshinImpact.GameTask;
+
+/// <summary>
+/// 通过自动任务的方式执行各种方法
+/// </summary>
+public class TaskRunner
+{
+    private readonly ILogger<TaskRunner> _logger = App.GetLogger<TaskRunner>();
+
+    // private readonly DispatcherTimerOperationEnum _timerOperation = DispatcherTimerOperationEnum.None;
+
+    private readonly string _name = string.Empty;
+
+    public TaskRunner()
+    {
+    }
+
+    // public TaskRunner(DispatcherTimerOperationEnum timerOperation)
+    // {
+    //     _timerOperation = timerOperation;
+    // }
+    
+    /// <summary>
+    /// 运行当前任务的方法
+    /// </summary>
+    /// <param name="action"></param>
+    /// <returns></returns>
+    public async Task RunCurrentAsync(Func<Task> action)
+    {
+        // 加锁
+        var hasLock = await TaskSemaphore.WaitAsync(0);
+        if (!hasLock)
+        {
+            _logger.LogError("获取锁失败，当前可能有正在运行的独立任务，请不要重复执行任务");
+            return;
+        }
+        try
+        {
+            _logger.LogInformation("→ {Text}", _name + "任务开始执行");
+
+            // 初始化
+            Init();
+            
+            CancellationContext.Instance.Set();
+            RunnerContext.Instance.Clear();
+
+            await action();
+        }
+        catch (NormalEndException e)
+        {
+            Notify.Event(NotificationEvent.TaskCancel).Success("notification.message.taskCancelNormal");
+            _logger.LogInformation("任务中断:{Msg}", e.Message);
+            if (RunnerContext.Instance.IsContinuousRunGroup)
+            {
+                // 连续执行时需要抛出异常来终止执行
+                throw;
+            }
+        }
+        catch (TaskCanceledException e)
+        {
+            Notify.Event(NotificationEvent.TaskCancel).Success("notification.message.taskCancelManual");
+            _logger.LogInformation("任务中断:{Msg}", "手动取消");
+            if (RunnerContext.Instance.IsContinuousRunGroup)
+            {
+                // 连续执行时需要抛出异常来终止执行
+                throw;
+            }
+        }
+        catch (Exception e)
+        {
+            Notify.Event(NotificationEvent.TaskError).Error("notification.error.taskExecution", e);
+            _logger.LogError(e.Message);
+            _logger.LogDebug(e.StackTrace);
+        }
+        finally
+        {
+            End();
+            _logger.LogInformation("← {Text}", _name + "任务结束");
+
+            CancellationContext.Instance.Clear();
+            RunnerContext.Instance.Clear();
+
+            // 释放锁
+            if (hasLock)
+            {
+                TaskSemaphore.Release();
+            }
+        }
+    }
+
+    public void FireAndForget(Func<Task> action)
+    {
+        Task.Run(() => RunCurrentAsync(action));
+    }
+
+    public async Task RunThreadAsync(Func<Task> action)
+    {
+        await Task.Run(() => RunCurrentAsync(action));
+    }
+
+    public async Task RunSoloTaskAsync(ISoloTask soloTask)
+    {
+        // 没有界面时不等待
+        bool waitForMainUi = soloTask.Name != "自动秘境挂机" && !soloTask.Name.Contains("自动钓鱼") && !soloTask.Name.Contains("模拟宇宙");
+        await ScriptService.StartGameTask(waitForMainUi);
+        await Task.Run(() => RunCurrentAsync(async () => await soloTask.Start(CancellationContext.Instance.Cts.Token)));
+    }
+
+    public void Init()
+    {
+        if (!TaskContext.Instance().IsInitialized)
+        {
+            UIDispatcherHelper.Invoke(() => { Toast.Warning("请先在启动页中，启动截图功能，再使用本功能"); });
+            throw new NormalEndException("请先在启动页中，启动截图功能，再使用本功能");
+        }
+
+        // 清空实时任务触发器
+        TaskTriggerDispatcher.Instance().ClearTriggers();
+
+        
+        // 显示遮罩窗口
+        var maskWindow = MaskWindow.Instance();
+        SystemControl.ActivateWindow();
+        maskWindow.Invoke(maskWindow.Show);
+    }
+
+    public void End()
+    {
+        if (!TaskContext.Instance().IsInitialized)
+        {
+            return;
+        }
+        
+        // 恢复实时任务触发器
+        TaskTriggerDispatcher.Instance().SetTriggers(GameTaskManager.LoadInitialTriggers());
+
+        VisionContext.Instance().DrawContent.ClearAll();
+    }
+
+}
