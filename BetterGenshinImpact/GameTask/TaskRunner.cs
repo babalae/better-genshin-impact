afd5842b008430bfc805b2009fd3594bc4383ff1
DR-lin-eng <52230594+DR-lin-eng@users.noreply.github.com>
2025-07-24 17:50:39 +0800

    Update TaskRunner.cs
    解决一些显示问题

--- BetterGenshinImpact/GameTask/TaskRunner.cs
+++ BetterGenshinImpact/GameTask/TaskRunner.cs
@@ -1,4 +1,4 @@
-﻿using BetterGenshinImpact.Core.Script;
+using BetterGenshinImpact.Core.Script;
 using BetterGenshinImpact.GameTask.AutoGeniusInvokation.Exception;
 
 using BetterGenshinImpact.View;
@@ -17,7 +17,7 @@ using BetterGenshinImpact.Service.Notification.Model.Enum;
 namespace BetterGenshinImpact.GameTask;
 
 /// <summary>
-/// 通过自动任务的方式执行各种方法
+/// 用于以独立任务的方式执行任意方法
 /// </summary>
 public class TaskRunner
 {
@@ -35,9 +35,9 @@ public class TaskRunner
     // {
     //     _timerOperation = timerOperation;
     // }
-    
+
     /// <summary>
-    /// 运行当前任务的方法
+    /// 加锁并独立运行任务
     /// </summary>
     /// <param name="action"></param>
     /// <returns></returns>
@@ -47,16 +47,16 @@ public class TaskRunner
         var hasLock = await TaskSemaphore.WaitAsync(0);
         if (!hasLock)
         {
-            _logger.LogError("获取锁失败，当前可能有正在运行的独立任务，请不要重复执行任务");
+            _logger.LogError("任务启动失败：当前存在正在运行中的独立任务，请不要重复执行任务！");
             return;
         }
         try
         {
-            _logger.LogInformation("→ {Text}", _name + "任务开始执行");
+            _logger.LogInformation("→ {Text}", _name + "任务启动！");
 
             // 初始化
             Init();
-            
+
             CancellationContext.Instance.Set();
             RunnerContext.Instance.Clear();
 
@@ -68,17 +68,17 @@ public class TaskRunner
             _logger.LogInformation("任务中断:{Msg}", e.Message);
             if (RunnerContext.Instance.IsContinuousRunGroup)
             {
-                // 连续执行时需要抛出异常来终止执行
+                // 连续执行时，抛出异常，终止执行
                 throw;
             }
         }
         catch (TaskCanceledException e)
         {
             Notify.Event(NotificationEvent.TaskCancel).Success("notification.message.taskCancelManual");
-            _logger.LogInformation("任务中断:{Msg}", "手动取消");
+            _logger.LogInformation("任务中断:{Msg}", "任务被取消");
             if (RunnerContext.Instance.IsContinuousRunGroup)
             {
-                // 连续执行时需要抛出异常来终止执行
+                // 连续执行时，抛出异常，终止执行
                 throw;
             }
         }
@@ -91,7 +91,7 @@ public class TaskRunner
         finally
         {
             End();
-            _logger.LogInformation("← {Text}", _name + "任务结束");
+            _logger.LogInformation("→ {Text}", _name + "任务结束");
 
             CancellationContext.Instance.Clear();
             RunnerContext.Instance.Clear();
@@ -116,8 +116,8 @@ public class TaskRunner
 
     public async Task RunSoloTaskAsync(ISoloTask soloTask)
     {
-        // 没有界面时不等待
-        bool waitForMainUi = soloTask.Name != "自动秘境挂机" && !soloTask.Name.Contains("自动钓鱼") && !soloTask.Name.Contains("模拟宇宙");
+        // 没启动的时候先启动
+        bool waitForMainUi = soloTask.Name != "自动七圣召唤" && !soloTask.Name.Contains("自动音游") && !soloTask.Name.Contains("幽境危战");
         await ScriptService.StartGameTask(waitForMainUi);
         await Task.Run(() => RunCurrentAsync(async () => await soloTask.Start(CancellationContext.Instance.Cts.Token)));
     }
@@ -126,15 +126,15 @@ public class TaskRunner
     {
         if (!TaskContext.Instance().IsInitialized)
         {
-            UIDispatcherHelper.Invoke(() => { Toast.Warning("请先在启动页中，启动截图功能，再使用本功能"); });
-            throw new NormalEndException("请先在启动页中，启动截图功能，再使用本功能");
+            UIDispatcherHelper.Invoke(() => { Toast.Warning("请先在启动页，启动截图器再使用本功能"); });
+            throw new NormalEndException("请先在启动页，启动截图器再使用本功能");
         }
 
         // 清空实时任务触发器
         TaskTriggerDispatcher.Instance().ClearTriggers();
 
-        
-        // 显示遮罩窗口
+
+        // 激活原神窗口
         var maskWindow = MaskWindow.Instance();
         SystemControl.ActivateWindow();
         maskWindow.Invoke(maskWindow.Show);
@@ -146,8 +146,8 @@ public class TaskRunner
         {
             return;
         }
-        
-        // 恢复实时任务触发器
+
+        // 还原实时任务触发器
         TaskTriggerDispatcher.Instance().SetTriggers(GameTaskManager.LoadInitialTriggers());
 
         VisionContext.Instance().DrawContent.ClearAll();
