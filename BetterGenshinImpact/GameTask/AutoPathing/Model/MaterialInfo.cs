8a46f9dcd969395e341f2c946ff9217f040914f1
辉鸭蛋 <huiyadanli@gmail.com>
2025-07-30 01:06:57 +0800

    地图追踪json添加更多属性和配置

---
+++ BetterGenshinImpact/GameTask/AutoPathing/Model/MaterialInfo.cs
@@ -0,0 +1,10 @@
+namespace BetterGenshinImpact.GameTask.AutoPathing.Model;
+
+public class MaterialInfo
+{
+    public string? Monster { get; set; }
+    
+    public string? Material { get; set; }
+    
+    public string? Count { get; set; }
+}
