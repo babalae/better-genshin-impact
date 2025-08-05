3e8701f7157e2a28782eb6b39fecad36c1166ca4
茶包 <kaedelcb@qq.com>
2025-08-05 08:00:43 +0800

    优化按键函数写法

--- BetterGenshinImpact/View/Pages/OneDragonFlowPage.xaml.cs
+++ BetterGenshinImpact/View/Pages/OneDragonFlowPage.xaml.cs
@@ -14,11 +14,14 @@ public partial class OneDragonFlowPage
     public OneDragonFlowViewModel ViewModel { get; }
     
     private readonly Dictionary<CheckBox, string> _checkBoxMappings;
+    
+    public static readonly List<string> SereniteaPotTypes = new List<string> { "每天重复", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日" };
 
     public OneDragonFlowPage(OneDragonFlowViewModel viewModel)
     {
         DataContext = ViewModel = viewModel;
         InitializeComponent();
+        
         _checkBoxMappings = new Dictionary<CheckBox, string>
         {
             { ClothCheckBox, "布匹" },
@@ -30,11 +33,34 @@ public partial class OneDragonFlowPage
             { ExpBottleBigCheckBox, "祝圣精华" },
             { ExpBottleSmallCheckBox, "祝圣油膏" }
         };
+        
     }
     
     private async void SereniteaPotTpType_Clicked(object sender, RoutedEventArgs e)
     {
-        if (ViewModel.SelectedConfig != null)
+        if (ViewModel.SelectedConfig == null)
+        {
+            Toast.Warning("初始化失败，请先选择配置单");
+            return;
+        }
+        
+        if (sender.Equals(PopupCloseButton)) //关闭弹窗
+        {
+            Popup.IsOpen = false;
+        }
+        
+        else if (sender.Equals(PopupConfirmButton)) //确认选择
+        {
+            var selectedObjects = new List<string>(SereniteaPotComboBox.SelectedItem.ToString().Split('/'))
+                .Concat(_checkBoxMappings.Where(pair => pair.Key.IsChecked == true).Select(pair => pair.Value)).ToList();
+
+            ViewModel.SelectedConfig.SecretTreasureObjects = selectedObjects;
+            ViewModel.SaveConfig();
+            
+            Popup.IsOpen = false;
+        }
+        
+        else if (sender.Equals(PotButton)) //打开初始化显示购买信息
         {
             if (ViewModel.SelectedConfig.SecretTreasureObjects.Count == 0)
             {
@@ -42,7 +68,6 @@ public partial class OneDragonFlowPage
                 ViewModel.SaveConfig();
                 return;
             }
-            
             SereniteaPotComboBox.SelectedItem = ViewModel.SelectedConfig.SecretTreasureObjects[0];
 
             foreach (var pair in _checkBoxMappings)
@@ -50,29 +75,7 @@ public partial class OneDragonFlowPage
                 pair.Key.IsChecked = ViewModel.SelectedConfig.SecretTreasureObjects.Contains(pair.Value);
             }
         }
-    }
-    
-    private void BuySereniteaPotClick(object sender, RoutedEventArgs e)
-    {
-        if (ViewModel.SelectedConfig == null || ViewModel.SelectedConfig.SecretTreasureObjects == null)
-        {
-            Toast.Warning("初始化失败，请先选择配置单");
-            return;
-        }
         
-        var selectedObjects = new List<string>(SereniteaPotComboBox.SelectedItem.ToString().Split('/'))
-            .Concat(_checkBoxMappings.Where(pair => pair.Key.IsChecked == true).Select(pair => pair.Value)).ToList();
-
-        ViewModel.SelectedConfig.SecretTreasureObjects = selectedObjects;
-        ViewModel.SaveConfig();
-
-        Toast.Success("添加购买成功");
-        Popup.IsOpen = false;
-    }
-    
-    private void PopupCloseButtonClick(object sender, RoutedEventArgs e)
-    {
-        Popup.IsOpen = false;
     }
     
 }
