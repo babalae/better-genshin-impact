2e04a75d5c0295d7af4d5d5d5e20a1018ea8512c
bhbghghbgb <113711814+bhbghghbgb@users.noreply.github.com>
2025-09-10 13:41:08 +0700

    expose server time zone offset to js return local DateTimeOffset for eaiser testing without Config

--- BetterGenshinImpact/Helpers/TimeZoneHelper.cs
+++ BetterGenshinImpact/Helpers/TimeZoneHelper.cs
@@ -3,8 +3,19 @@ using BetterGenshinImpact.GameTask;
 
 namespace BetterGenshinImpact.Helpers;
 
-public class TimeZoneHelper
+public static class TimeZoneHelper
 {
-    public static DateTimeOffset GetServerTimeNow() =>
-        DateTimeOffset.UtcNow.ToOffset(TaskContext.Instance().Config.OtherConfig.ServerTimeZoneOffset);
+    public static DateTimeOffset GetServerTimeNow()
+    {
+        try
+        {
+            return DateTimeOffset.UtcNow.ToOffset(TaskContext.Instance().Config.OtherConfig.ServerTimeZoneOffset);
+        }
+        // throw new Exception("Config未初始化"); in TaskContext.cs
+        catch (Exception)
+        {
+            // Assume Beijing timezone for core developers' testing 
+            return DateTimeOffset.Now;
+        }
+    }
 }
