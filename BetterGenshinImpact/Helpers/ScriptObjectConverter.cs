d7a1cde198b86574c5fe43025f9dc8ba2753f5e3
FishmanTheMurloc <checkitout@yeah.net>
2025-09-25 20:41:40 +0800

    ScriptObjectConverter.GetValue()新增支持一层集合泛型参数解析；CountInventoryItem新增支持复数物品的计数，并在Dispatcher中向JavaScript暴露此种使用方式

--- BetterGenshinImpact/Helpers/ScriptObjectConverter.cs
+++ BetterGenshinImpact/Helpers/ScriptObjectConverter.cs
@@ -1,5 +1,6 @@
 using Microsoft.ClearScript;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace BetterGenshinImpact.Helpers;
@@ -44,41 +45,108 @@ public class ScriptObjectConverter
     {
         if (source[propertyName] is not Undefined && source[propertyName] != null)
         {
-            object value = source.GetProperty(propertyName);
+            object v8Value = source.GetProperty(propertyName);
 
-            Type type = typeof(T);
-            type = Nullable.GetUnderlyingType(type) ?? type;
-
-            if (type.IsEnum)
+            if (TryMap(v8Value, out T value))
             {
-                // 处理数字
-                if (value is int intValue)
-                {
-                    if (Enum.IsDefined(type, intValue))
-                    {
-                        return (T)Enum.ToObject(type, intValue);
-                    }
-                    else
-                    {
-                        return defaultValue;
-                    }
-                }
-                // 处理字符串
-                else if (value is string strValue)
-                {
-                    if (Enum.TryParse(type, strValue, ignoreCase: true, out object? parsedEnum))
-                    {
-                        return (T)parsedEnum;
-                    }
-                    else
-                    {
-                        return defaultValue;
-                    }
-                }
+                return value;
             }
-
-            return (T)value;
         }
         return defaultValue;
+    }
+
+    /// <summary>
+    /// <para>适用集合的重载</para>
+    /// 如果<paramref name="propertyName"/>解析失败，默认返回一个空集合；
+    /// 如果集合元素解析失败，将跳过该元素
+    /// <para>仅支持一层集合，<typeparamref name="T"/>不能再是集合</para>
+    /// <para>避开反射，享受健康生活</para>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="source"></param>
+    /// <param name="propertyName"></param>
+    /// <returns></returns>
+    public static IEnumerable<T> GetValue<T>(ScriptObject source, string propertyName)
+    {
+        if (source[propertyName] is not Undefined && source[propertyName] != null)
+        {
+            object v8Value = source.GetProperty(propertyName);
+
+            return TryMap<T>(v8Value);
+        }
+        return new T[0];
+    }
+
+    /// <summary>
+    /// 尝试将ClearScript的类型转换成常用类型
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="v8Value"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    private static bool TryMap<T>(object v8Value, out T value)
+    {
+        Type type = typeof(T);
+        type = Nullable.GetUnderlyingType(type) ?? type;
+
+        if (type.IsEnum)
+        {
+            // 处理数字
+            if (v8Value is int intValue)
+            {
+                if (Enum.IsDefined(type, intValue))
+                {
+                    value = (T)Enum.ToObject(type, intValue);
+                    return true;
+                }
+                else
+                {
+                    value = default!;
+                    return false;
+                }
+            }
+            // 处理字符串
+            else if (v8Value is string strValue)
+            {
+                if (Enum.TryParse(type, strValue, ignoreCase: true, out object? parsedEnum))
+                {
+                    value = (T)parsedEnum;
+                    return true;
+                }
+                else
+                {
+                    value = default!;
+                    return false;
+                }
+            }
+        }
+
+        value = (T)v8Value;
+        return true;
+    }
+
+    /// <summary>
+    /// 尝试将ClearScript的V8Array类型转换成常用泛型集合类型
+    /// 如果集合元素解析失败，将跳过该元素，如此始终能返回一个集合
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="v8Value"></param>
+    /// <returns></returns>
+    private static IEnumerable<T> TryMap<T>(object v8Value)
+    {
+        Type elementType = typeof(T);
+        var iList = (System.Collections.IList)v8Value;  // V8Array虽然是私有类型无法获取，但其实现IList，可根据此接口操作
+
+        Type listType = typeof(List<>).MakeGenericType(elementType);
+        System.Collections.IList list = (System.Collections.IList)Activator.CreateInstance(listType)!;
+        foreach (var elementV8Value in iList)
+        {
+            if (TryMap(elementV8Value, out T elementValue))
+            {
+                list.Add(elementValue);
+            }
+        }
+
+        return (IEnumerable<T>)list;
     }
 }
