db8bde1d7457c77ca93cfb6addbcf59cbbfe978b
Misty <gyc990326@gmail.com>
2025-10-13 03:04:06 +0800

    feat: 改进http，返回header和status_code，并实现url级别细粒度控制和ui提示

--- BetterGenshinImpact/Service/ScriptService.cs
+++ BetterGenshinImpact/Service/ScriptService.cs
@@ -486,7 +486,7 @@ public partial class ScriptService : IScriptService
         target.JsScriptSettingsObject = source.JsScriptSettingsObject;
         target.GroupInfo = source.GroupInfo;
         target.AllowJsNotification = source.AllowJsNotification;
-        target.AllowJsHTTP = source.AllowJsHTTP;
+        target.AllowJsHTTPHash = source.AllowJsHTTPHash;
         target.SkipFlag = source.SkipFlag;
     }
 
