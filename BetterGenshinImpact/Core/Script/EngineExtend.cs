f19a16e1d3ef79a0eac4a1edf05d50f5406b4114
DarkFlameMaster <1004452714@qq.com>
2025-12-01 00:40:04 +0800

    增加模块导入的支持 (#2494)

--- BetterGenshinImpact/Core/Script/EngineExtend.cs
+++ BetterGenshinImpact/Core/Script/EngineExtend.cs
@@ -1,13 +1,14 @@
-﻿using BetterGenshinImpact.Core.Script.Dependence;
+﻿using System;
+using System.Collections.Generic;
+using BetterGenshinImpact.Core.Script.Dependence;
 using BetterGenshinImpact.Core.Script.Dependence.Model;
 using Microsoft.ClearScript;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using OpenCvSharp;
 using BetterGenshinImpact.Core.Recognition;
 using BetterGenshinImpact.GameTask.Model.Area;
-using BetterGenshinImpact.Core.Config;
+using BetterGenshinImpact.Core.Script.Utils;
 using BetterGenshinImpact.GameTask.AutoDomain;
 using BetterGenshinImpact.GameTask.AutoFight;
 using BetterGenshinImpact.GameTask.AutoFight.Model;
@@ -75,13 +76,30 @@ public class EngineExtend
         // 添加C#的类型
         engine.AddHostType(typeof(Task));
 
-        // 导入 CommonJS 模块
+        // 导入 JavaScript 模块
         // https://microsoft.github.io/ClearScript/2023/01/24/module-interop.html
         // https://github.com/microsoft/ClearScript/blob/master/ClearScriptTest/V8ModuleTest.cs
         engine.DocumentSettings.AccessFlags = DocumentAccessFlags.EnableFileLoading | DocumentAccessFlags.AllowCategoryMismatch;
         if (searchPaths != null)
         {
-            engine.DocumentSettings.SearchPath = string.Join(';', searchPaths);
+            var normalizedPaths = new List<string>();
+            foreach (var path in searchPaths)
+            {
+                try
+                {
+                    var normalizedPath = ScriptUtils.NormalizePath(workDir, path);
+                    normalizedPaths.Add(normalizedPath);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"从 library 字段读取路径 '{path}' 失败: {ex.Message}", ex);
+                }
+            }
+
+            if (normalizedPaths.Count > 0)
+            {
+                engine.DocumentSettings.SearchPath = string.Join(';', normalizedPaths);
+            }
         }
     }
 
