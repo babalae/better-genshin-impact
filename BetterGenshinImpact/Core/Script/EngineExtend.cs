cd5b22548c20cb53aa76b99ba6c9df8181855537
DarkFlameMaster <1004452714@qq.com>
2026-01-03 16:24:53 +0800

    feat:JS添加自动剧情的实时任务时，支持传入参数 (#2591)

--- BetterGenshinImpact/Core/Script/EngineExtend.cs
+++ BetterGenshinImpact/Core/Script/EngineExtend.cs
@@ -12,6 +12,7 @@ using BetterGenshinImpact.Core.Script.Utils;
 using BetterGenshinImpact.GameTask.AutoDomain;
 using BetterGenshinImpact.GameTask.AutoFight;
 using BetterGenshinImpact.GameTask.AutoFight.Model;
+using BetterGenshinImpact.GameTask.AutoSkip;
 
 namespace BetterGenshinImpact.Core.Script;
 
@@ -40,6 +41,7 @@ public class EngineExtend
         engine.AddHostObject("dispatcher", new Dispatcher(config));
         engine.AddHostType("RealtimeTimer", typeof(RealtimeTimer));
         engine.AddHostType("SoloTask", typeof(SoloTask));
+        engine.AddHostType("AutoSkipConfig", typeof(AutoSkipConfig));
         
         // 添加取消令牌相关类型
         engine.AddHostType("CancellationTokenSource", typeof(CancellationTokenSource));
