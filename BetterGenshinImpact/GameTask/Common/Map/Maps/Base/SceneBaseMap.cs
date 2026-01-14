b8546511e263b520c0d202807e80d5a02fdc66d1
Jamis <JamisHoo@users.noreply.github.com>
2026-01-14 15:24:28 +0800

    Fix SIFT regression

--- BetterGenshinImpact/GameTask/Common/Map/Maps/Base/SceneBaseMap.cs
+++ BetterGenshinImpact/GameTask/Common/Map/Maps/Base/SceneBaseMap.cs
@@ -102,7 +102,7 @@ public abstract class SceneBaseMap : ISceneMap
         {
             try
             {
-                var result = SiftMatcher.KnnMatch(layer.TrainKeyPoints, layer.TrainDescriptors, greyMiniMapMat, null, DescriptorMatcherType.BruteForce);
+                var result = SiftMatcher.KnnMatch(layer.TrainKeyPoints, layer.TrainDescriptors, greyMiniMapMat);
                 if (result != default)
                 {
                     return result;
