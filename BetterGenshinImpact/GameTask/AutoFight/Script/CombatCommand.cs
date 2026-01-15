fac0287301f011200094f2eafaceee0c943a785b
躁动的氨气 <3193156071@qq.com>
2026-01-15 16:01:11 +0800

    战斗策略添加状态检测

--- BetterGenshinImpact/GameTask/AutoFight/Script/CombatCommand.cs
+++ BetterGenshinImpact/GameTask/AutoFight/Script/CombatCommand.cs
@@ -192,6 +192,10 @@ public class CombatCommand
             var s = double.Parse(Args![0]);
             avatar.Wait((int)TimeSpan.FromSeconds(s).TotalMilliseconds);
         }
+        else if (Method == Method.WaitForDone)
+        {
+            avatar.WaitForDone();
+        }
         else if (Method == Method.Aim)
         {
             throw new NotImplementedException();
