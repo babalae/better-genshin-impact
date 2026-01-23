de79d8a61ca43e381811871c6b5f93df1dd2138a
躁动的氨气 <131591012+zaodonganqi@users.noreply.github.com>
2026-01-24 00:45:05 +0800

    feat: 添加进出千星以刷新CD (#2677)

--- BetterGenshinImpact/GameTask/AutoPathing/PathExecutor.cs
+++ BetterGenshinImpact/GameTask/AutoPathing/PathExecutor.cs
@@ -1091,6 +1091,7 @@ public class PathExecutor
             || waypoint.Action == ActionEnum.Mining.Code
             || waypoint.Action == ActionEnum.Fishing.Code
             || waypoint.Action == ActionEnum.ExitAndRelogin.Code
+            || waypoint.Action == ActionEnum.EnterAndExitWonderland.Code
             || waypoint.Action == ActionEnum.SetTime.Code
             || waypoint.Action == ActionEnum.UseGadget.Code
             || waypoint.Action == ActionEnum.PickUpCollect.Code)
