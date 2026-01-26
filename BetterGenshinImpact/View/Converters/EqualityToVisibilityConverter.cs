e17603b20c9afdd9d9622c3ba9377dfa27e9612d
DR-lin-eng <@DR-lin-eng>
2026-01-26 21:24:22 +0800

    残留未推送文件。

---
+++ BetterGenshinImpact/View/Converters/EqualityToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace BetterGenshinImpact.View.Converters;
+
+public class EqualityToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null || parameter == null)
+        {
+            return Visibility.Collapsed;
+        }
+
+        return value.ToString() == parameter.ToString() ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
