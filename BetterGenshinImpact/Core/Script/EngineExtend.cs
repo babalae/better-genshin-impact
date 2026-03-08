3c13bc8bd7a7a02ec3972ea0a97b4ada48ad4c5c
DarkFlameMaster <1004452714@qq.com>
2026-03-08 18:37:03 +0800

    feat(JS): 支持调用 C#中含out/ref参数的方法

--- BetterGenshinImpact/Core/Script/EngineExtend.cs
+++ BetterGenshinImpact/Core/Script/EngineExtend.cs
@@ -85,6 +85,7 @@ public class EngineExtend
         engine.AddHostType("BvLocator", typeof(BvLocator));
         engine.AddHostType("BvImage", typeof(BvImage));
 
+        engine.AddHostObject("host", new HostFunctions());
 
         // 导入 JavaScript 模块
         // https://microsoft.github.io/ClearScript/2023/01/24/module-interop.html
