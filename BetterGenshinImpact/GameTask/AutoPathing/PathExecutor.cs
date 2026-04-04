5a665a7f5795637521e00295014b71b688582620
秋云 <physligl@gmail.com>
2026-04-04 23:26:03 +0800

    Refactor input handling in PathExecutor to use SimulateAction method
    - Replaced direct key up calls for movement and mouse actions with SimulateAction method for better abstraction and maintainability.
    - Updated EndAction invocation to use EndJudgment for improved clarity in action handling.

--- BetterGenshinImpact/GameTask/AutoPathing/PathExecutor.cs
+++ BetterGenshinImpact/GameTask/AutoPathing/PathExecutor.cs
@@ -105,7 +105,7 @@ public partial class PathExecutor
             _trapEscaper,
             pathExecutorSuspend,
             () => CaptureToRectArea(),
-            img => EndAction?.Invoke(img) ?? false,
+            EndJudgment,
             ResolveAnomalies,
             WaitUntilRotatedTo,
             index => SwitchAvatar(index),
@@ -273,8 +273,8 @@ public partial class PathExecutor
                     finally
                     {
                         // 不管咋样，松开所有按键
-                        Simulation.SendInput.Keyboard.KeyUp(User32.VK.VK_W);
-                        Simulation.SendInput.Mouse.RightButtonUp();
+                        Simulation.SendInput.SimulateAction(GIActions.MoveForward, KeyType.KeyUp);
+                        Simulation.SendInput.SimulateAction(GIActions.SprintMouse, KeyType.KeyUp);
                     }
                 }
             }
