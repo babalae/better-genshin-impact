2a73293f633c51b678fe98ef6daadfc543ba7216
cznull <32773496+cznull@users.noreply.github.com>
2026-04-09 20:43:26 +0800

    Update AutoFightTask.cs

--- BetterGenshinImpact/GameTask/AutoFight/AutoFightTask.cs
+++ BetterGenshinImpact/GameTask/AutoFight/AutoFightTask.cs
@@ -761,7 +761,7 @@ public class AutoFightTask : ISoloTask
             bool? result = null;
             try
             {
-                result = await AutoFightSeek.SeekAndFightAsync(Logger, detectDelayTime, delayTime, _ct);
+                result = await AutoFightSeek.SeekAndFightAsync(Logger, detectDelayTime, delayTime, _ct, false, _taskParam.RotaryFactor);
             }
             catch (Exception ex)
             {
