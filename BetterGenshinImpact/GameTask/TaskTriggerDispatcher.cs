0eedfb696951b03f37834120501229888154a989
辉鸭蛋 <huiyadanli@gmail.com>
2026-04-16 00:29:56 +0800

    实时任务中对遮罩的操作异步化

--- BetterGenshinImpact/GameTask/TaskTriggerDispatcher.cs
+++ BetterGenshinImpact/GameTask/TaskTriggerDispatcher.cs
@@ -315,7 +315,7 @@ namespace BetterGenshinImpact.GameTask
                     PictureInPictureService.Hide(resetManual: true);
                     // if (!_prevGameActive)
                     // {
-                    maskWindow.Invoke(() =>
+                    maskWindow.BeginInvoke(() =>
                     {
                         if (maskWindow.IsExist())
                         {
