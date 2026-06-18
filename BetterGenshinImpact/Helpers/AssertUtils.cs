a5bcee176c7fec14f73460bd5e9a4ae383011c97
KuaiCode <434900025@qq.com>
2026-06-19 00:21:25 +0800

    修复千音雅集全屏黑边场景的分辨率校验

--- BetterGenshinImpact/Helpers/AssertUtils.cs
+++ BetterGenshinImpact/Helpers/AssertUtils.cs
@@ -2,6 +2,7 @@
 using BetterGenshinImpact.GameTask;
 using BetterGenshinImpact.GameTask.Common;
 using Microsoft.Extensions.Logging;
+using Vanara.PInvoke;
 
 namespace BetterGenshinImpact.Helpers;
 
@@ -22,11 +23,19 @@ public class AssertUtils
     /// </summary>
     public static void CheckGameResolution(string msg = "")
     {
-        var gameScreenSize = SystemControl.GetGameScreenRect(TaskContext.Instance().GameHandle);
-        if (gameScreenSize.Width * 9 != gameScreenSize.Height * 16)
+        var taskContext = TaskContext.Instance();
+        var gameScreenSize = taskContext is { IsInitialized: true, SystemInfo: not null }
+            ? taskContext.SystemInfo.GameScreenSize
+            : SystemControl.GetGameScreenRect(taskContext.GameHandle);
+        if (!IsGameResolution16X9(gameScreenSize))
         {
             TaskControl.Logger.LogError("游戏窗口分辨率不是 16:9 ！当前分辨率为 {Width}x{Height} , 非 16:9 分辨率的游戏无法正常使用{Msg}功能 !", gameScreenSize.Width, gameScreenSize.Height, msg);
             throw new Exception("游戏窗口分辨率不是 16:9");
         }
+    }
+
+    public static bool IsGameResolution16X9(RECT gameScreenSize)
+    {
+        return gameScreenSize.Width * 9 == gameScreenSize.Height * 16;
     }
 }
--- Test/BetterGenshinImpact.UnitTest/GameTaskTests/CaptureGeometryTests.cs
+++ Test/BetterGenshinImpact.UnitTest/GameTaskTests/CaptureGeometryTests.cs
@@ -1,6 +1,9 @@
+using BetterGenshinImpact.GameTask;
 using BetterGenshinImpact.GameTask.Model;
 using BetterGenshinImpact.GameTask.Model.Area;
+using BetterGenshinImpact.Helpers;
 using BetterGenshinImpact.UnitTest.GameTaskTests.AutoFishingTests;
+using System;
 using OpenCvSharp;
 using Vanara.PInvoke;
 
@@ -41,5 +44,68 @@ public class CaptureGeometryTests
 
         Assert.Equal(1280, x);
         Assert.Equal(800, y);
+    }
+
+    [Fact]
+    public void CheckGameResolution_UsesContentSizeWhenTaskContextInitialized()
+    {
+        var systemInfo = new FakeSystemInfo(new RECT(0, 0, 2560, 1600), 1);
+
+        RunWithInitializedSystemInfo(systemInfo, () => AssertUtils.CheckGameResolution("自动音游"));
+
+        Assert.Equal(new RECT(0, 0, 2560, 1440), systemInfo.GameScreenSize);
+    }
+
+    [Fact]
+    public void CheckGameResolution_AllowsNative16x9Capture()
+    {
+        var systemInfo = new FakeSystemInfo(new RECT(0, 0, 1920, 1080), 1);
+
+        RunWithInitializedSystemInfo(systemInfo, () => AssertUtils.CheckGameResolution("自动音游"));
+    }
+
+    [Fact]
+    public void IsGameResolution16X9_ReturnsFalseWhenDerivedContentSizeIsNot16x9()
+    {
+        var systemInfo = new FakeSystemInfo(new RECT(0, 0, 1000, 1000), 1);
+
+        Assert.False(AssertUtils.IsGameResolution16X9(systemInfo.GameScreenSize));
+    }
+
+    [Fact]
+    public void ScaledContentRegion_Converts1080PMusicPointBackToRawGameCapturePoint()
+    {
+        var rawMat = new Mat(1600, 2560, MatType.CV_8UC3);
+        var desktopRegion = new DesktopRegion(2560, 1600, new FakeMouseSimulator());
+        using var rawRegion = desktopRegion.Derive(rawMat, 0, 0);
+        using var contentRegion = rawRegion.DeriveCrop(new Rect(0, 80, 2560, 1440));
+        using var scaledRegion = contentRegion.DeriveTo1080P();
+
+        var (x, y) = scaledRegion.ConvertPositionToGameCaptureRegion(960, 921);
+
+        Assert.Equal(1280, x);
+        Assert.Equal(1308, y);
+    }
+
+    private static void RunWithInitializedSystemInfo(ISystemInfo systemInfo, Action action)
+    {
+        var taskContext = TaskContext.Instance();
+        var originalIsInitialized = taskContext.IsInitialized;
+        var originalGameHandle = taskContext.GameHandle;
+        var originalSystemInfo = taskContext.SystemInfo;
+
+        try
+        {
+            taskContext.IsInitialized = true;
+            taskContext.GameHandle = IntPtr.Zero;
+            taskContext.SystemInfo = systemInfo;
+            action();
+        }
+        finally
+        {
+            taskContext.IsInitialized = originalIsInitialized;
+            taskContext.GameHandle = originalGameHandle;
+            taskContext.SystemInfo = originalSystemInfo;
+        }
     }
 }
