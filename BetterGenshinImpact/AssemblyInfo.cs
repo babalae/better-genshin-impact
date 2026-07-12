f121745c4ba5a7db0473410aa2976b8fdf81391b
辉鸭蛋 <huiyadanli@gmail.com>
2026-07-13 03:05:16 +0800

    还原

--- BetterGenshinImpact/AssemblyInfo.cs
+++ BetterGenshinImpact/AssemblyInfo.cs
@@ -1,7 +1,5 @@
-using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Media;
 
-[assembly: InternalsVisibleTo("BetterGenshinImpact.UnitTest")]
 [assembly: DisableDpiAwareness]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
