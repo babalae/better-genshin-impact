87e9e2c7fe7a829bf07414ca295ebad4adeea9b4
辉鸭蛋 <huiyadanli@gmail.com>
2026-07-28 03:22:41 +0800

    Revert "解决多实例的日志问题"
    This reverts commit 59d01d4f6f421341c6824c4937ec98dd31e60766.

--- BetterGenshinImpact/App.xaml.cs
+++ BetterGenshinImpact/App.xaml.cs
@@ -66,31 +66,26 @@ public partial class App : Application
                 var logFolder = Path.Combine(AppContext.BaseDirectory, "log");
                 Directory.CreateDirectory(logFolder);
                 var logFile = Path.Combine(logFolder, "better-genshin-impact.log");
-                var instanceContext = InstanceBootstrap.Current.Context;
-                var instanceIdentity =
-                    $"{instanceContext.InstanceType}:S{instanceContext.WindowsSessionId}:P{instanceContext.ProcessId}:T{instanceContext.StartedAt.ToUnixTimeMilliseconds()}";
 
                 var richTextBox = new RichTextBoxImpl();
                 services.AddSingleton<IRichTextBox>(richTextBox);
 
                 var loggerConfiguration = new LoggerConfiguration()
-                    .Enrich.WithProperty("BgiInstance", instanceIdentity)
                     .WriteTo.File(logFile,
                         outputTemplate:
-                        "[{Timestamp:HH:mm:ss.fff}] [{Level:u3}] [{BgiInstance}] {SourceContext}{NewLine}{Message}{NewLine}{Exception}{NewLine}",
+                        "[{Timestamp:HH:mm:ss.fff}] [{Level:u3}] {SourceContext}{NewLine}{Message}{NewLine}{Exception}{NewLine}",
                         rollingInterval: RollingInterval.Day,
-                        shared: true,
-                        retainedFileCountLimit: null,
+                        retainedFileCountLimit: 31,
                         retainedFileTimeLimit: TimeSpan.FromDays(21))
                     .WriteTo.Console(outputTemplate:
-                        "[{Timestamp:HH:mm:ss} {Level:u3}] [{BgiInstance}] {Message:lj}{NewLine}{Exception}")
+                        "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
                     .MinimumLevel.Debug()
                     .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
                     .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Warning);
                 if (all.MaskWindowConfig is { MaskEnabled: true, ShowLogBox: true })
                 {
                     loggerConfiguration.WriteTo.RichTextBox(richTextBox, LogEventLevel.Information,
-                        "[{Timestamp:HH:mm:ss} {Level:u3}] [{BgiInstance}] {Message:lj}{NewLine}{Exception}");
+                        "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}");
                 }
 
                 Log.Logger = loggerConfiguration.CreateLogger();
@@ -277,7 +272,6 @@ public partial class App : Application
 
         await _host.StopAsync();
         _host.Dispose();
-        Log.CloseAndFlush();
 
         // 释放控制台窗口
         ConsoleHelper.FreeConsoleWindow();
--- BetterGenshinImpact/GameTask/LogParse/LogParse.cs
+++ BetterGenshinImpact/GameTask/LogParse/LogParse.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -19,9 +19,6 @@ namespace BetterGenshinImpact.GameTask.LogParse
     {
         private static readonly string _configPath = Global.Absolute(@"log\logparse\config.json");
         private static readonly string _assets_dir = Global.Absolute($@"GameTask\LogParse\Assets");
-        private static readonly Regex LogHeaderRegex = new(
-            @"^\[\d{2}:\d{2}:\d{2}\.\d+\] \[[^\]]+\](?: \[(?<Instance>[^\]]+)\])?",
-            RegexOptions.Compiled | RegexOptions.CultureInvariant);
         // 添加一个静态事件用于通知日志的生成状态
         public static event Action<string> HtmlGenerationStatusChanged = delegate { };
         private static void NotifyHtmlGenerationStatus(string status)
@@ -52,35 +49,17 @@ namespace BetterGenshinImpact.GameTask.LogParse
 
         public static List<ConfigGroupEntity> ParseFile(List<(string, string)> logFiles)
         {
-            Dictionary<string, List<(string, string)>> instanceLogLines = new(StringComparer.Ordinal);
+            List<(string, string)> logLines = new();
             foreach (var logFile in logFiles)
             {
-                string? currentInstance = null;
-                foreach (var logLine in SafeReadAllLines(logFile.Item1))
+                string[] logstrs = SafeReadAllLines(logFile.Item1).ToArray();
+                foreach (var logstr in logstrs)
                 {
-                    var headerMatch = LogHeaderRegex.Match(logLine);
-                    if (headerMatch.Success)
-                    {
-                        var instanceGroup = headerMatch.Groups["Instance"];
-                        currentInstance = instanceGroup.Success ? instanceGroup.Value : null;
-                        if (currentInstance is not null
-                            && !instanceLogLines.ContainsKey(currentInstance))
-                        {
-                            instanceLogLines[currentInstance] = [];
-                        }
-                    }
-
-                    if (currentInstance is not null)
-                    {
-                        instanceLogLines[currentInstance].Add((logLine, logFile.Item2));
-                    }
+                    logLines.Add((logstr, logFile.Item2));
                 }
             }
 
-            return instanceLogLines.Values
-                .SelectMany(Parse)
-                .OrderBy(entity => entity.StartDate)
-                .ToList();
+            return Parse(logLines);
         }
 
         public static List<ConfigGroupEntity> Parse(List<(string, string)> logLines)
@@ -325,7 +304,7 @@ namespace BetterGenshinImpact.GameTask.LogParse
             }
 
             // 定义文件名匹配的正则表达式
-            string pattern = @"^better-genshin-impact(\d{8})\.log$";
+            string pattern = @"^better-genshin-impact(\d{8})(_\d{3})*\.log$";
             Regex regex = new Regex(pattern);
 
             // 遍历文件夹中的所有文件
@@ -344,7 +323,7 @@ namespace BetterGenshinImpact.GameTask.LogParse
                     if (DateTime.TryParseExact(dateString, "yyyyMMdd", null, DateTimeStyles.None,
                             out DateTime parsedDate))
                     {
-                        result.Add((Path.Combine(folderPath, fileName), parsedDate.ToString("yyyy-MM-dd")));
+                        result.Add((folderPath + "\\" + fileName, parsedDate.ToString("yyyy-MM-dd")));
                     }
                 }
             }
