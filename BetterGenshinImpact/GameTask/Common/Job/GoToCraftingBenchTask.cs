5b9aa2eb1e398dcbd11f7038d5e5e450abd05c7d
DarkFlameMaster <1004452714@qq.com>
2026-08-01 17:11:43 +0800

    拆分 前往合成台 与 前往合成树脂 方法 (#3364)

--- BetterGenshinImpact/GameTask/Common/Job/GoToCraftingBenchTask.cs
+++ BetterGenshinImpact/GameTask/Common/Job/GoToCraftingBenchTask.cs
@@ -45,14 +45,14 @@ public class GoToCraftingBenchTask
         this.craftLocalizedString = stringLocalizer.WithCultureGet(cultureInfo, "合成");
     }
     
-    public async Task Start(string country, CancellationToken ct)
+    public async Task GoCraftResin(string country, CancellationToken ct)
     {
         Logger.LogInformation("→ {Name} 开始", Name);
         for (int i = 0; i < _retryTimes; i++)
         {
             try
             {
-                await DoOnce(country, ct);
+                await GoCraftResinOnce(country, ct);
                 break;
             }
             catch (Exception e)
@@ -74,17 +74,11 @@ public class GoToCraftingBenchTask
         Logger.LogInformation("→ {Name} 结束", Name);
     }
 
-    public async Task DoOnce(string country, CancellationToken ct)
+    public async Task GoCraftResinOnce(string country, CancellationToken ct)
     {
          // 1. 走到合成台并交互
-        await GoToCraftingBench(country, ct);
+        await GoToCraftingBenchOnce(country, ct);
 
-        // 2. 等待合成界面
-        await _chooseTalkOptionTask.SelectLastOptionUntilEnd(ct,
-            region => region.Find(ElementRecognition.Get("BtnWhiteConfirm", region)).IsExist()
-        );
-        await Delay(800, ct);
-        
         // 判断浓缩树脂是否存在
         // TODO 满的情况是怎么样子的
         var ra = CaptureToRectArea();
@@ -207,6 +201,32 @@ public class GoToCraftingBenchTask
     /// <returns></returns>
     public async Task GoToCraftingBench(string country, CancellationToken ct)
     {
+        Logger.LogInformation("→ {Name} 开始", Name);
+        for (int i = 0; i < _retryTimes; i++)
+        {
+            try
+            {
+                await GoToCraftingBenchOnce(country, ct);
+                break;
+            }
+            catch (Exception e)
+            {
+                Logger.LogError("前往合成台执行异常：" + e.Message);
+                if (i == _retryTimes - 1)
+                {
+                    throw;
+                }
+
+                await Delay(1000, ct);
+                Logger.LogInformation("重试前往合成台");
+            }
+        }
+
+        Logger.LogInformation("→ {Name} 结束", Name);
+    }
+
+    public async Task GoToCraftingBenchOnce(string country, CancellationToken ct)
+    {
         var task = PathingTask.BuildFromFilePath(Global.Absolute(@$"GameTask\Common\Element\Assets\Json\合成台_{country}.json"));
         if (task == null)
         {
@@ -250,6 +270,12 @@ public class GoToCraftingBenchTask
             
             }
         }
+
+        // 等待进入合成界面
+        await _chooseTalkOptionTask.SelectLastOptionUntilEnd(ct,
+            region => region.Find(ElementRecognition.Get("BtnWhiteConfirm", region)).IsExist()
+        );
+        await Delay(800, ct);
     }
 
 
