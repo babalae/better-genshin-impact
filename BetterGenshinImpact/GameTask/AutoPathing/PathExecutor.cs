3948b8e3dbda28fad519a5e24a0b909dd6fc6906
mno <718135749@qq.com>
2026-08-08 15:25:44 +0800

    Revert "fix(AutoPathing): 玛薇卡车上禁用冲刺时显式抬起冲刺键并重置fastMode，避免上车后持续冲刺消耗夜魂值"
    This reverts commit 82e10934e3fd5d48cb1229a46d7e83123ea6da33.

--- BetterGenshinImpact/GameTask/AutoPathing/PathExecutor.cs
+++ BetterGenshinImpact/GameTask/AutoPathing/PathExecutor.cs
@@ -744,10 +744,6 @@ public partial class PathExecutor
     }
 
     public DateTime moveToStartTime;
-    /// <summary>
-    /// Run 路段疾跑按键（SprintMouse KeyDown）状态，供赶路逻辑（如玛薇卡车上禁用冲刺）读取并重置
-    /// </summary>
-    private bool _fastMode;
 
     public async Task MoveTo(WaypointForTrack waypoint)
     {
@@ -761,7 +757,7 @@ public partial class PathExecutor
         await WaitUntilRotatedTo(targetOrientation, 5);
         moveToStartTime = DateTime.UtcNow;
         var lastPositionRecord = DateTime.UtcNow;
-        _fastMode = false;
+        var fastMode = false;
         var prevPositions = new List<Point2f>();
         var fastModeColdTime = DateTime.MinValue;
         var prevNotTooFarPosition = position;
@@ -944,9 +940,9 @@ public partial class PathExecutor
             // 只有设置为run才会一直疾跑
             if (waypoint.MoveMode == MoveModeEnum.Run.Code)
             {
-                if (distance > 20 != _fastMode) // 距离大于20时可以使用疾跑/自由泳
+                if (distance > 20 != fastMode) // 距离大于20时可以使用疾跑/自由泳
                 {
-                    if (_fastMode)
+                    if (fastMode)
                     {
                         Simulation.SendInput.SimulateAction(GIActions.SprintMouse, KeyType.KeyUp);
                     }
@@ -955,7 +951,7 @@ public partial class PathExecutor
                         Simulation.SendInput.SimulateAction(GIActions.SprintMouse, KeyType.KeyDown);
                     }
 
-                    _fastMode = !_fastMode;
+                    fastMode = !fastMode;
                 }
             }
             else if (waypoint.MoveMode == MoveModeEnum.Dash.Code)
