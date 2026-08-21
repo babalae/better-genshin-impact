73ada6c1f5a411d1a0836b85554749cd0bf5343b
guamian <92618050+guamian1337@users.noreply.github.com>
2026-08-22 04:44:58 +0800

    新增禁用键鼠监听

--- BetterGenshinImpact/Core/Monitor/MouseKeyMonitor.cs
+++ BetterGenshinImpact/Core/Monitor/MouseKeyMonitor.cs
@@ -91,6 +91,13 @@ public partial class MouseKeyMonitor : IDisposable
         var fi = TaskContext.Instance().Config.MacroConfig.FFireInterval;
         _fTimer.Interval = fi;
 
+        // 开关禁用时不装低级钩子
+        if (TaskContext.Instance().Config.DisableInputMonitor)
+        {
+            if (_isSubscribed) Unsubscribe();
+            return;
+        }
+
         if (_isSubscribed)
         {
             return;
