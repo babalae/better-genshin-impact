05b074ccd4e108a2899689c801b625cf08ff1576
辉鸭蛋 <huiyadanli@gmail.com>
2026-09-15 02:21:02 +0800

    Revert "fix: 提高背包尘歌壶进入流程可靠性" (#3672)

--- BetterGenshinImpact/GameTask/Common/Job/GoToSereniteaPotTask.cs
+++ BetterGenshinImpact/GameTask/Common/Job/GoToSereniteaPotTask.cs
@@ -200,27 +200,12 @@ internal class GoToSereniteaPotTask
     private async Task<bool> IntoSereniteaPotByBag(CancellationToken ct)
     {
         // 尝试使用背包的壶进入。
-        if (!await QuickSereniteaPotTask.Start(ct))
-        {
-            Logger.LogWarning("领取尘歌壶奖励:通过背包触发进入尘歌壶失败");
-            return false;
-        }
-
+        QuickSereniteaPotTask.Done();
         await Delay(5000, ct); // 在点击壶之后的特殊加载页面会有 mainUI
-        if (!await Bv.WaitForMainUi(ct))
-        {
-            Logger.LogWarning("领取尘歌壶奖励:进入尘歌壶后未检测到主界面");
-            return false;
-        }
-
+        await Bv.WaitForMainUi(ct);
         // 判断是否在尘歌壶中
-        var fingerIconFound = await NewRetry.WaitForAction(() =>
-        {
-            using var capture = CaptureToRectArea();
-            using var fingerIcon = capture.Find(ElementRecognition.Get("FingerIcon", capture));
-            return fingerIcon.IsExist();
-        }, ct, 5, 500);
-        if (fingerIconFound)
+        using var ra0 = CaptureToRectArea();
+        if (ra0.Find(ElementRecognition.Get("FingerIcon", ra0)).IsExist())
         {
             await Delay(1000, ct);
             // 尝试获取尘歌壶名称
